e402fb5eafaaa75f2b0ddacdf3cd9359d872469e
Alexander Neundorf <neundorf@kde.org>
2006-01-27 22:44:32 +0000

    -first *complete* build of *complete* kdelibs, with no undefined symbols left -compile fix in interfaces/ktexteditor/codecompletion2.h, gcc 3.3.6 didn't compile it: In file included from /home/alex/src/kde4-svn/kdelibs/kate/part/kateview.h:31,                  from /home/alex/src/kde4-svn/kdelibs/kate/part/katesearch.cpp:27: /home/alex/src/kde4-svn/kdelibs/interfaces/ktexteditor/codecompletion2.h:89: error: duplicate field enum KTextEditor::CodeCompletionModel::HighlightMethod' (as enum and non-enum) /home/alex/src/kde4-svn/kdelibs/kate/part/katesearch.cpp: In constructor  KateReplacePrompt::KateRep lacePrompt(QWidget*)': /home/alex/src/kde4-svn/kdelibs/kate/part/katesearch.cpp:628: warning:   __base_ctor' is deprecated (declared at /home/alex/src/kde4-svn/kdelibs/kdeui/kdialogbase.h:194)
    -compile fix in kdeui/ktip.cpp, gcc 3.3.6 didn't compile it
    
    Alex
    
    CCMAIL: kwrite-devel@kde.org
    
    
    svn path=/trunk/KDE/kdelibs/; revision=503028

---
+++ modules/FindGIF.cmake
@@ -0,0 +1,35 @@
+# - Try to find GIF
+# Once done this will define
+#
+#  GIF_FOUND - system has GIF
+#  GIF_INCLUDE_DIR - the GIF include directory
+#  GIF_LIBRARY - Link these to use OpenGL and GLU
+#  GIF_DEFINITIONS - Compiler switches required for using GIF
+#
+
+
+FIND_PATH(GIF_INCLUDE_DIR gif_lib.h
+  /usr/include
+  /usr/local/include
+)
+
+FIND_LIBRARY(GIF_LIBRARY NAMES ungif
+  PATHS
+  /usr/lib
+  /usr/local/lib
+)
+
+IF(GIF_INCLUDE_DIR AND GIF_LIBRARY)
+   SET(GIF_FOUND TRUE)
+ENDIF(GIF_INCLUDE_DIR AND GIF_LIBRARY)
+
+IF(GIF_FOUND)
+  IF(NOT GIF_FIND_QUIETLY)
+    MESSAGE(STATUS "Found GIF: ${GIF_LIBRARY}")
+  ENDIF(NOT GIF_FIND_QUIETLY)
+ELSE(GIF_FOUND)
+  IF(GIF_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find GIF")
+  ENDIF(GIF_FIND_REQUIRED)
+ENDIF(GIF_FOUND)
+
