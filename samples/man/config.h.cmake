65afee80faf2956ee92a6d0584ad3bf96514b1d3
Alexander Neundorf <neundorf@kde.org>
2006-01-27 16:51:24 +0000

    -applying the patch by Bill Hoffman: change all #cmakedefine HAVE_FOO and #cmakedefine01 HAVE_FOO to #cmakedefine HAVE_FOO 1
    -> compatible with the current buildsystem
    
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=502950

--- samples/man/config.h.cmake
+++ samples/man/config.h.cmake
@@ -4,8 +4,8 @@
 #cmakedefine HAVE_UNISTD_H
 #cmakedefine HAVE_STRING_H
 
-#cmakedefine01 HAVE_BLAH_H
-#cmakedefine01 HAVE_UNISTD_H
+#cmakedefine HAVE_BLAH_H 1
+#cmakedefine HAVE_UNISTD_H 1
 
 
 #endif
