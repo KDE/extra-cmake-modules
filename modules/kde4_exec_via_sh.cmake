abdca3e2a38cf7e8fe615823ced9b194478948b7
Alexander Neundorf <neundorf@kde.org>
2006-04-08 18:09:04 +0000

    -try to make it work with paths which contain spaces
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=527571

--- modules/kde4_exec_via_sh.cmake
+++ modules/kde4_exec_via_sh.cmake
@@ -2,5 +2,5 @@
 # created by cmake, don't edit, changes will be lost
 
 
-${_library_path_variable}=${_ld_library_path}:$${_library_path_variable} ${_executable} $@
+${_library_path_variable}="${_ld_library_path}":$${_library_path_variable} ${_executable} $@
 
