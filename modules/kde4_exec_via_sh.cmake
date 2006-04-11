e13a70a79582b8dbe37f979ee4516558877ece48
Dirk Mueller <mueller@kde.org>
2006-04-11 23:48:23 +0000

    the usual security fixes: - add proper quoting - add proper LD_LIBRARY_PATH export's - don't make world executable. Ok, it would be better to   honor umask, but I don't know how to do that..
    svn path=/trunk/KDE/kdelibs/; revision=528894

--- modules/kde4_exec_via_sh.cmake
+++ modules/kde4_exec_via_sh.cmake
@@ -3,9 +3,9 @@ file(WRITE "${_filename}"
 "#!/bin/sh
 # created by cmake, don't edit, changes will be lost
 
-${_library_path_variable}=${_ld_library_path}:$${_library_path_variable} \"${_executable}\" $@
+${_library_path_variable}=${_ld_library_path}\${${_library_path_variable}+:${_library_path_variable}} \"${_executable}\" \"$@\"
 ")
 
 # make it executable
 # since this is only executed on UNIX, it is safe to call chmod
-exec_program(chmod ARGS 755 \"${_filename}\" OUTPUT_VARIABLE _dummy )
+exec_program(chmod ARGS ug+x \"${_filename}\" OUTPUT_VARIABLE _dummy )
