7d381d23c0fd7bc9f444880c8a5fb0c7cff1bc6a
David Faure <faure@kde.org>
2006-04-13 16:07:18 +0000

    Fix up include guard since it's missing UI_ currently
    svn path=/trunk/KDE/kdelibs/; revision=529505

--- modules/kde4uic.cmake
+++ modules/kde4uic.cmake
@@ -42,6 +42,9 @@ STRING(REGEX REPLACE "tr2i18n\\(\"\"\\)" "QString::null" _uic_CONTENTS "${_uic_C
 STRING(REGEX REPLACE "tr2i18n\\(\"\", \"\"\\)" "QString::null" _uic_CONTENTS "${_uic_CONTENTS}" )
 #replace image15_data with img15_filename to make enable_final work
 STRING(REGEX REPLACE "image([0-9]+)_data" "img\\1_${KDE_UIC_BASENAME}" _uic_CONTENTS "${_uic_CONTENTS}")
+#fixup include guard
+STRING(REGEX REPLACE "#ifndef " "#ifndef UI_" _uic_CONTENTS "${_uic_CONTENTS}")
+STRING(REGEX REPLACE "#define " "#define UI_" _uic_CONTENTS "${_uic_CONTENTS}")
 
 FILE(WRITE ${KDE_UIC_CPP_FILE} "#include <kdialog.h>\n#include <klocale.h>\n\n${_uic_CONTENTS}\n")
 
