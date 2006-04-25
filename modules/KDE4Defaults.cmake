e9a477bcfa7553bded8d7cb750a802d0d79915bf
Alexander Neundorf <neundorf@kde.org>
2006-04-25 20:43:22 +0000

    -add the new option to put source-local include dirs first, won't have any effect befor we require cmake 2.4.1 (we're still at 2.3.4)
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=533904

--- modules/KDE4Defaults.cmake
+++ modules/KDE4Defaults.cmake
@@ -4,13 +4,22 @@ enable_testing()
 include(CTest)
 
 # Always include srcdir and builddir in include path
+# This saves typing ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY} in about every subdir
+# since cmake 2.4.0
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
-# Use colors
+# put the include dirs which are in the source or build tree
+# before all other include dirs, so the headers in the sources
+# are prefered over the already installed ones
+# since cmake 2.4.1
+set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
+
+# Use colored output
+# since cmake 2.4.0
 set(CMAKE_COLOR_MAKEFILE ON)
 
 # define the generic version of the libraries here
-# this makes it easy to advance it when the next release comes
+# this makes it easy to advance it when the next KDE release comes
 set(GENERIC_LIB_VERSION "5.0.0")
 set(GENERIC_LIB_SOVERSION "5")
 
