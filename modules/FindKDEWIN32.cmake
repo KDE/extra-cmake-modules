14f067d7391b81094423fd71eb0b57920036eccf
Paulo Moura Guedes <moura@kdewebdev.org>
2006-05-15 15:21:32 +0000

    This is valid not only for msvc.
    CCMAIL: Peter Kummel [syntheticpp@gmx.net]
    
    svn path=/trunk/KDE/kdelibs/; revision=541109

--- modules/FindKDEWIN32.cmake
+++ modules/FindKDEWIN32.cmake
@@ -20,11 +20,11 @@ FIND_PATH(KDEWIN32_INCLUDE_DIR winposix_export.h
 
 # at first find the kdewin32 library, this has to be compiled and installed before kdelibs/
 # search for kdewin32 in the default install directory for applications (default of (n)make install)
-if (MSVC AND CMAKE_BUILD_TYPE MATCHES Debug)
+if (CMAKE_BUILD_TYPE MATCHES Debug)
    set(_KDEWIN32_POSTFIX d)
-else (MSVC AND CMAKE_BUILD_TYPE MATCHES Debug)
+else (CMAKE_BUILD_TYPE MATCHES Debug)
    set(_KDEWIN32_POSTFIX )
-endif (MSVC AND CMAKE_BUILD_TYPE MATCHES Debug)
+endif (CMAKE_BUILD_TYPE MATCHES Debug)
 
 FIND_LIBRARY(KDEWIN32_LIBRARY NAMES kdewin32${_KDEWIN32_POSTFIX}
   PATHS
