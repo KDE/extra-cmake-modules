62180630e416ded01918973edcaf6fe8936fd34d
Alexander Neundorf <neundorf@kde.org>
2006-06-09 21:15:15 +0000

    -also check for zdll on windows, patch from Peter -sync with cmake cvs
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=549790

--- modules/FindZLIB.cmake
+++ modules/FindZLIB.cmake
@@ -1,10 +1,9 @@
-# - Try to find a z compression library
-# under Windows the zlib exists with a variety of slightly different names
-# Once done this will define
+# - Find zlib
+# Find the native ZLIB includes and library
 #
-#  ZLIB_FOUND - system has zlib
-#  ZLIB_INCLUDE_DIR - the zlib include directory
-#  ZLIB_LIBRARY - The libraries needed to use zlib
+#  ZLIB_INCLUDE_DIR - where to find zlib.h, etc.
+#  ZLIB_LIBRARIES   - List of libraries when using zlib.
+#  ZLIB_FOUND       - True if zlib found.
 
 
 IF (ZLIB_INCLUDE_DIR)
@@ -13,33 +12,36 @@ IF (ZLIB_INCLUDE_DIR)
 ENDIF (ZLIB_INCLUDE_DIR)
 
 FIND_PATH(ZLIB_INCLUDE_DIR zlib.h
- /usr/include
- /usr/local/include
+  /usr/local/include
+  /usr/include
 )
 
-SET(POTENTIAL_Z_LIBS z zlib zdll)
-FIND_LIBRARY(ZLIB_LIBRARY NAMES ${POTENTIAL_Z_LIBS}
-   PATHS
-   /usr/lib
-   /usr/local/lib
+SET(ZLIB_NAMES z zlib zdll)
+FIND_LIBRARY(ZLIB_LIBRARY 
+   NAMES ${ZLIB_NAMES}
+   PATHS /usr/lib /usr/local/lib
 )
 
 IF (ZLIB_INCLUDE_DIR AND ZLIB_LIBRARY)
    SET(ZLIB_FOUND TRUE)
+   SET( ZLIB_LIBRARIES ${ZLIB_LIBRARY} )
 ELSE (ZLIB_INCLUDE_DIR AND ZLIB_LIBRARY)
    SET(ZLIB_FOUND FALSE)
+   SET( ZLIB_LIBRARIES )
 ENDIF (ZLIB_INCLUDE_DIR AND ZLIB_LIBRARY)
 
 IF (ZLIB_FOUND)
    IF (NOT ZLIB_FIND_QUIETLY)
-      MESSAGE(STATUS "Found Z: ${ZLIB_LIBRARY}")
+      MESSAGE(STATUS "Found ZLIB: ${ZLIB_LIBRARY}")
    ENDIF (NOT ZLIB_FIND_QUIETLY)
 ELSE (ZLIB_FOUND)
    IF (ZLIB_FIND_REQUIRED)
-      MESSAGE(STATUS "Looked for Z libraries named ${POTENTIAL_Z_LIBS}.")
-      MESSAGE(STATUS "Found no acceptable Z library. This is fatal.")
+      MESSAGE(STATUS "Looked for Z libraries named ${ZLIBS_NAMES}.")
       MESSAGE(FATAL_ERROR "Could NOT find z library")
    ENDIF (ZLIB_FIND_REQUIRED)
 ENDIF (ZLIB_FOUND)
 
-MARK_AS_ADVANCED(ZLIB_LIBRARY ZLIB_INCLUDE_DIR)
+MARK_AS_ADVANCED(
+  ZLIB_LIBRARY
+  ZLIB_INCLUDE_DIR
+  )
