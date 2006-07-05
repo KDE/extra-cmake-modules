ed05031001cff71b2358d3c9189fc8b685122d56
Allen Winter <winter@kde.org>
2006-07-05 19:22:34 +0000

    merge the settings from FindKDEPIMLibs.cmake, which I will be removing ASAP.
    svn path=/trunk/KDE/kdelibs/; revision=558658

--- modules/FindKdepimLibs.cmake
+++ modules/FindKdepimLibs.cmake
@@ -1,9 +1,14 @@
 # Find if we installed kdepimlibs before to compile it
 
+#TODO: use kcal/kcal.h, but it doesn't exist yet
 FIND_PATH( KDEPIMLIBS_INCLUDE_DIR emailfunctions/email.h 
   ${KDE4_INCLUDE_DIR}
 )
 
 if( NOT KDEPIMLIBS_INCLUDE_DIR )
-	message( FATAL_ERROR "You didn't compile kdepimlibs before to compile it. Please install it" )
+  message(FATAL_ERROR "Could NOT find a kdepimlibs installation.\nPlease build and install kdepimlibs first.")
 endif(NOT KDEPIMLIBS_INCLUDE_DIR )
+
+set(KDE4_EMAILFUNCTIONS_LIBS emailfunctions)
+set(KDE4_KCAL_LIBS kcal)
+set(KDE4_KTNEF_LIBS ktnef)
