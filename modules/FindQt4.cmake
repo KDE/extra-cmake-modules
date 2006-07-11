b797823de2557e12cebf464ce5c54095a30e8647
Laurent Montel <montel@kde.org>
2006-07-11 15:12:27 +0000

    Allow to specify classname. It's necessary to fix karm compile it uses same xml file but two different class and use the same lib =>it defined same object => we can't link. (it's very specific case)
    svn path=/trunk/KDE/kdelibs/; revision=560969

--- modules/FindQt4.cmake
+++ modules/FindQt4.cmake
@@ -894,14 +894,22 @@ IF (QT4_QMAKE_FOUND)
        STRING(TOLOWER ${_basename} _basename)
     ENDIF (_optionalBasename)
 
-    SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
+    SET(_optionalClassName "${ARGV5}")
+	SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
     SET(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
     SET(_moc    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
-
-    ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
+	
+	IF(_optionalClassName)
+       ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
+          COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -c ${_optionalClassName} -i ${_include} -l ${_parentClass} ${_infile}
+          DEPENDS ${_infile}
+        )			
+	ELSE(_optionalClassName)
+       ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
           COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -i ${_include} -l ${_parentClass} ${_infile}
           DEPENDS ${_infile}
         )
+	ENDIF(_optionalClassName)
 
     QT4_GENERATE_MOC(${_header} ${_moc})
     SET_SOURCE_FILES_PROPERTIES(${_impl} PROPERTIES SKIP_AUTOMOC TRUE)
