de179a0b561c77a2fe8cdfab39b724b7c0ce63f4
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2006-07-15 09:22:46 +0000

    fix for OpenSSL 0.9.8b with msvc hope it will work with < 0.9.8b too
    svn path=/trunk/KDE/kdelibs/; revision=562569

--- modules/FindOpenSSL.cmake
+++ modules/FindOpenSSL.cmake
@@ -16,17 +16,43 @@ else (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
      /usr/include/
      /usr/local/include/
   )
-  
-  FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32
-     PATHS
-     /usr/lib
-     /usr/local/lib
-  )
-  
+
+  if(WIN32 AND MSVC)
+      # /MD and /MDd are the standard values - if somone wants to use
+      # others, the libnames have to change here too
+
+      FIND_LIBRARY(SSL_EAY_DEBUG NAMES ssleay32MDd )
+      FIND_LIBRARY(SSL_EAY_RELEASE NAMES ssleay32MD ssl ssleay32)
+
+      IF(MSVC_IDE)
+        IF(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+            SET(OPENSSL_LIBRARIES optimized ${SSL_EAY_RELEASE} debug ${SSL_EAY_DEBUG})
+        ELSE(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+          MESSAGE(FATAL_ERROR "Could not find the debug and release version of openssl")
+        ENDIF(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+      ELSE(MSVC_IDE)
+        STRING(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
+        IF(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+          SET(OPENSSL_LIBRARIES ${SSL_EAY_DEBUG})
+        ELSE(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+          SET(OPENSSL_LIBRARIES ${SSL_EAY_RELEASE})
+        ENDIF(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+      ENDIF(MSVC_IDE)
+      MARK_AS_ADVANCED(SSL_EAY_DEBUG SSL_EAY_RELEASE)
+  else(WIN32 AND MSVC)
+
+   FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32 ssleay32MD
+      PATHS
+      /usr/lib
+      /usr/local/lib
+   )
+
+  endif(WIN32 AND MSVC)
+
   if (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
      set(OPENSSL_FOUND TRUE)
   endif (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
-  
+
   if (OPENSSL_FOUND)
      if (NOT OpenSSL_FIND_QUIETLY)
         message(STATUS "Found OpenSSL: ${OPENSSL_LIBRARIES}")
@@ -36,7 +62,7 @@ else (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
         message(FATAL_ERROR "Could NOT find OpenSSL")
      endif (OpenSSL_FIND_REQUIRED)
   endif (OPENSSL_FOUND)
-  
+
   MARK_AS_ADVANCED(OPENSSL_INCLUDE_DIR OPENSSL_LIBRARIES)
-  
+
 endif (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
