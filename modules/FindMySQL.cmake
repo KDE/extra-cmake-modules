3e0547ea2a57dcf96d319a6597903aec8628f437
Tobias Koenig <tokoe@kde.org>
2006-09-29 11:36:01 +0000

    Add support for finding embedded version of MySql
    svn path=/trunk/KDE/kdelibs/; revision=590147

--- modules/FindMySQL.cmake
+++ modules/FindMySQL.cmake
@@ -1,9 +1,11 @@
-# - Find MySQL
+# - Find MySQL / MySQL Embedded
 # Find the MySQL includes and client library
 # This module defines
 #  MYSQL_INCLUDE_DIR, where to find mysql.h
 #  MYSQL_LIBRARIES, the libraries needed to use MySQL.
+#  MYSQL_EMBEDDED_LIBRARIES, the libraries needed to use MySQL Embedded.
 #  MYSQL_FOUND, If false, do not try to use MySQL.
+#  MYSQL_EMBEDDED_FOUND, If false, do not try to use MySQL Embedded.
 #
 # Copyright (c) 2006, Jaroslaw Staniek, <js@iidea.pl>
 #
@@ -22,6 +24,13 @@ find_library(MYSQL_LIBRARIES NAMES mysqlclient
    /usr/local/lib/mysql
 )
 
+find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld
+   PATHS
+   /usr/lib/mysql
+   /usr/local/lib/mysql
+   /opt/mysql/lib/mysql
+)
+
 if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
    set(MYSQL_FOUND TRUE)
    message(STATUS "Found MySQL: ${MYSQL_INCLUDE_DIR}, ${MYSQL_LIBRARIES}")
@@ -30,5 +39,12 @@ else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
    message(STATUS "MySQL not found.")
 endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
 
-mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES)
+if(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES)
+   set(MYSQL_EMBEDDED_FOUND TRUE)
+   message(STATUS "Found MySQL Embedded: ${MYSQL_INCLUDE_DIR}, ${MYSQL_EMBEDDED_LIBRARIES}")
+else(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES)
+   set(MYSQL_EMBEDDED_FOUND FALSE)
+   message(STATUS "MySQL Embedded not found.")
+endif(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES)
 
+mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES MYSQL_EMBEDDED_LIBRARIES)
