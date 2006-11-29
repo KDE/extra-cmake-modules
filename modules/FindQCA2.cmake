7541066130fa7d4a57875b9c6d95f6e169a7f853
Michel Hermier <michel.hermier@gmail.com>
2006-11-29 11:42:36 +0000

    No objections after 2 days in the mailling list, commiting then.
    svn path=/trunk/KDE/kdelibs/; revision=609041

--- modules/FindQCA2.cmake
+++ modules/FindQCA2.cmake
@@ -14,42 +14,49 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-
-
-INCLUDE(UsePkgConfig)
-IF (NOT WIN32)
-PKGCONFIG(qca _Qca2IncDir _Qca2LinkDir _Qca2LinkFlags _Qca2Cflags)
-
-MESSAGE(STATUS "pkg-config returned ${_Qca2IncDir} for QCA 2 includes")
-set(QCA2_DEFINITIONS ${_Qca2Cflags})
-
-set(QCA2_INCLUDE_DIR ${_Qca2IncDir})
-ENDIF (NOT WIN32)
-#FIND_PATH(QCA2_INCLUDE_DIR QtCrypto
-#  PATHS
-# ${_Qca2IncDir}
-#)
-
-MESSAGE(STATUS "QCA2_INCLUDE_DIR has value ${QCA2_INCLUDE_DIR}" )
-
-FIND_LIBRARY(QCA2_LIBRARIES NAMES qca
-  PATHS
-  ${_Qca2LinkDir} 
-  NO_DEFAULT_PATH 
-)
-
 if (QCA2_INCLUDE_DIR AND QCA2_LIBRARIES)
-   set(QCA2_FOUND TRUE)
+
+  # in cache allready
+  set(QCA2_FOUND TRUE)  
+
+else (QCA2_INCLUDE_DIR AND QCA2_LIBRARIES)
+
+  INCLUDE(UsePkgConfig)
+
+  IF (NOT WIN32)
+    PKGCONFIG(qca _Qca2IncDir _Qca2LinkDir _Qca2LinkFlags _Qca2Cflags)
+
+    MESSAGE(STATUS "pkg-config returned ${_Qca2IncDir} for QCA 2 includes")
+    set(QCA2_DEFINITIONS ${_Qca2Cflags})
+  ENDIF (NOT WIN32)
+
+  FIND_PATH(QCA2_INCLUDE_DIR QtCrypto
+    PATHS
+    ${_Qca2IncDir}
+    )
+
+  MESSAGE(STATUS "QCA2_INCLUDE_DIR has value ${QCA2_INCLUDE_DIR}" )
+
+  FIND_LIBRARY(QCA2_LIBRARIES NAMES qca
+    PATHS
+    ${_Qca2LinkDir} 
+    NO_DEFAULT_PATH 
+  )
+
+  if (QCA2_INCLUDE_DIR AND QCA2_LIBRARIES)
+    set(QCA2_FOUND TRUE)
+  endif (QCA2_INCLUDE_DIR AND QCA2_LIBRARIES)
+
+  if (QCA2_FOUND)
+    if (NOT QCA2_FIND_QUIETLY)
+      message(STATUS "Found QCA2: ${QCA2_LIBRARIES}")
+    endif (NOT QCA2_FIND_QUIETLY)
+  else (QCA2_FOUND)
+    if (QCA2_FIND_REQUIRED)
+      message(SEND_ERROR "Could NOT find QCA2")
+    endif (QCA2_FIND_REQUIRED)
+  endif (QCA2_FOUND)
+
+  MARK_AS_ADVANCED(QCA2_INCLUDE_DIR QCA2_LIBRARIES)
+
 endif (QCA2_INCLUDE_DIR AND QCA2_LIBRARIES)
-
-if (QCA2_FOUND)
-  if (NOT QCA2_FIND_QUIETLY)
-    message(STATUS "Found QCA2: ${QCA2_LIBRARIES}")
-  endif (NOT QCA2_FIND_QUIETLY)
-else (QCA2_FOUND)
-  if (QCA2_FIND_REQUIRED)
-    message(SEND_ERROR "Could NOT find QCA2")
-  endif (QCA2_FIND_REQUIRED)
-endif (QCA2_FOUND)
-
-MARK_AS_ADVANCED(QCA2_INCLUDE_DIR QCA2_LIBRARIES)
