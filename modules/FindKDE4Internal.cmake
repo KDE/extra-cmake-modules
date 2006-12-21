64b8ff4a5be27de456b5f7f9d11c092339513517
David Faure <faure@kde.org>
2006-12-21 13:05:56 +0000

    That part is fine.
    svn path=/trunk/KDE/kdelibs/; revision=615408

--- modules/FindKDE4Internal.cmake
+++ modules/FindKDE4Internal.cmake
@@ -700,7 +700,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
    # get the gcc version
    exec_program(${CMAKE_C_COMPILER} ARGS --version OUTPUT_VARIABLE _gcc_version_info)
 
-   string (REGEX MATCH " [34]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
+   string (REGEX MATCH "[345]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
    # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the patch level, handle this here:
    if (NOT _gcc_version)
       string (REGEX REPLACE ".*\\(GCC\\).* ([34]\\.[0-9]) .*" "\\1.0" _gcc_version "${_gcc_version_info}")
