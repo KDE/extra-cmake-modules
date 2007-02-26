79a60305da192e7fd1983516d51dea24630add14
Ralf Habacker <ralf.habacker@freenet.de>
2007-02-26 14:37:26 +0000

    - added more real library search on win32
    svn path=/trunk/KDE/kdelibs/; revision=637460

--- modules/FindKdepimLibs.cmake
+++ modules/FindKdepimLibs.cmake
@@ -12,9 +12,9 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if (WIN32)
-		file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _progFiles)
-		set (KDE4_INCLUDE_DIR ${KDE4_INCLUDE_DIR} ${_progFiles}/kdepimlibs/include)
-		set (KDE4_LIB_DIR ${KDE4_LIB_DIR} ${_progFiles}/kdepimlibs/lib)
+  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _progFiles)
+  set (KDE4_INCLUDE_DIR ${KDE4_INCLUDE_DIR} ${_progFiles}/kdepimlibs/include)
+  set (KDE4_LIB_DIR ${KDE4_LIB_DIR} ${_progFiles}/kdepimlibs/lib)
 endif (WIN32)
 
 find_path( KDEPIMLIBS_INCLUDE_DIR kcal/kcal.h
@@ -23,15 +23,42 @@ find_path( KDEPIMLIBS_INCLUDE_DIR kcal/kcal.h
 
 if( KDEPIMLIBS_INCLUDE_DIR )
   set(KDEPIMLIBS_FOUND TRUE)
-  find_library(KDE4_EMAILFUNCTIONS_LIBS emailfunctions PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_KABC_LIBS kabc PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
+  if (WIN32)
+    find_library(KDE4_KABC_LIBRARY NAMES kabc PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KABC_LIBS ${kabc_LIB_DEPENDS} ${KDE4_KABC_LIBRARY} )
 
-  find_library(KDE4_KCAL_LIBS kcal PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_KTNEF_LIBS ktnef PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_KRESOURCES_LIBS kresources PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_SYNDICATION_LIBS syndication PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_KLDAP_LIBS kldap PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
-  find_library(KDE4_KMIME_LIBS kmime PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH)
+    find_library(KDE4_EMAILFUNCTIONS_LIBRARY NAMES emailfunctions PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_EMAILFUNCTIONS_LIBS ${emailfunctions_LIB_DEPENDS} ${KDE4_EMAILFUNCTIONS_LIBRARY} )
+
+    find_library(KDE4_KCAL_LIBRARY NAMES kcal PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KCAL_LIBS ${kcal_LIB_DEPENDS} ${KDE4_KCAL_LIBRARY} )
+
+    find_library(KDE4_KTNEF_LIBRARY NAMES ktnef PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KTNEF_LIBS ${ktnef_LIB_DEPENDS} ${KDE4_KTNEF_LIBRARY} )
+
+    find_library(KDE4_KRESOURCES_LIBRARY NAMES kresources PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KRESOURCES_LIBS ${kresources_LIB_DEPENDS} ${KDE4_KRESOURCES_LIBRARY} )
+
+    find_library(KDE4_SYNDICATION_LIBRARY NAMES syndication PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_SYNDICATION_LIBS ${syndication_LIB_DEPENDS} ${KDE4_SYNDICATION_LIBRARY} )
+
+    find_library(KDE4_KLDAP_LIBRARY NAMES kldap PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KLDAP_LIBS ${kldap_LIB_DEPENDS} ${KDE4_KLDAP_LIBRARY} )
+
+    find_library(KDE4_KMIME_LIBRARY NAMES kmime PATHS ${KDE4_LIB_DIR} NO_DEFAULT_PATH )
+    set(KDE4_KMIME_LIBS ${kmime_LIB_DEPENDS} ${KDE4_KMIME_LIBRARY} )
+
+  else (WIN32)
+    set(KDE4_EMAILFUNCTIONS_LIBS emailfunctions)
+    set(KDE4_KABC_LIBS kabc)
+    set(KDE4_KCAL_LIBS kcal)
+    set(KDE4_KTNEF_LIBS ktnef)
+    set(KDE4_KRESOURCES_LIBS kresources)
+    set(KDE4_SYNDICATION_LIBS syndication)
+    set(KDE4_KLDAP_LIBS kldap)
+    set(KDE4_KMIME_LIBS kmime)
+  endif(WIN32)
+
   # setup global used KDE include
   set (KDE4_INCLUDES ${KDE4_INCLUDES} ${KDEPIMLIBS_INCLUDE_DIR})
 else( KDEPIMLIBS_INCLUDE_DIR )
