a7f051430a3d525a60ecb1c7584c049824b774dd
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-05-01 10:07:18 +0000

    search for debug/release lib on windows
    svn path=/trunk/KDE/kdelibs/; revision=660009

--- modules/FindSoprano.cmake
+++ modules/FindSoprano.cmake
@@ -26,10 +26,37 @@ else(SOPRANO_INCLUDE_DIR AND SOPRANO_LIBRARIES)
     ${KDE4_LIB_DIR}
     ${LIB_INSTALL_DIR}
     )
-
   if(SOPRANO_INCLUDE_DIR AND SOPRANO_LIBRARIES)
     set(Soprano_FOUND TRUE)
   endif(SOPRANO_INCLUDE_DIR AND SOPRANO_LIBRARIES)
+
+  if(MSVC)
+    FIND_LIBRARY(SOPRANO_LIBRARIES_DEBUG 
+      NAMES
+      sopranod
+      PATHS
+      /usr/lib
+      /usr/local/lib
+      ${KDE4_LIB_DIR}
+      ${LIB_INSTALL_DIR}
+      )
+    if(NOT SOPRANO_LIBRARIES_DEBUG)
+      set(Soprano_FOUND FALSE)
+    endif(NOT SOPRANO_LIBRARIES_DEBUG)
+    
+    if(MSVC_IDE)
+      if( NOT SOPRANO_LIBRARIES_DEBUG OR NOT SOPRANO_LIBRARIES)
+        message(FATAL_ERROR "\nCould NOT find the debug AND release version of the Soprano library.\nYou need to have both to use MSVC projects.\nPlease build and install both soprano libraries first.\n")
+      endif( NOT SOPRANO_LIBRARIES_DEBUG OR NOT SOPRANO_LIBRARIES)
+    else(MSVC_IDE)
+      string(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
+      if(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+        set(SOPRANO_LIBRARIES ${SOPRANO_LIBRARIES_DEBUG})
+      else(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+        set(SOPRANO_LIBRARIES ${SOPRANO_LIBRARIES})
+      endif(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+    endif(MSVC_IDE)
+  endif(MSVC)
 
   if(Soprano_FOUND)
     if(NOT Soprano_FIND_QUIETLY)
