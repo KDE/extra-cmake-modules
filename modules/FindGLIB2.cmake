f078487d1b9e273d2b2af81cbaba6f1b6f65f92e
Alexander Neundorf <neundorf@kde.org>
2007-08-07 02:11:45 +0000

    -use PATH_SUFFIXES in FindGLIB2.cmake to make the command even shorter -remove unecessary search path for msgfmt
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=697172

--- modules/FindGLIB2.cmake
+++ modules/FindGLIB2.cmake
@@ -9,35 +9,21 @@ if(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
     # Already in cache, be silent
     set(GLIB2_FIND_QUIETLY TRUE)
 endif(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
-IF (NOT WIN32)
-   INCLUDE(UsePkgConfig)
-   PKGCONFIG(glib-2.0 _LibGLIB2IncDir _LibGLIB2LinkDir _LibGLIB2LinkFlags _LibGLIB2Cflags)
+
+if (NOT WIN32)
+   include(UsePkgConfig)
+   pkgconfig(glib-2.0 _LibGLIB2IncDir _LibGLIB2LinkDir _LibGLIB2LinkFlags _LibGLIB2Cflags)
 endif(NOT WIN32)
 
-FIND_PATH(GLIB2_INCLUDE_DIR glib.h
-   ${_LibGLIB2IncDir}
-   ${_LibGLIB2IncDir}/glib-2.0
-   /usr/include/glib-2.0
-   /usr/local/include/glib-2.0
-)
+find_path(GLIB2_INCLUDE_DIR glib.h
+          PATH_SUFFIXES glib-2.0
+          PATHS ${_LibGLIB2IncDir} )
 
-FIND_LIBRARY(GLIB2_LIBRARIES NAMES glib-2.0
-    PATHS
-    ${_LibGLIB2LinkDir}
-)
+find_library(GLIB2_LIBRARIES 
+             NAMES glib-2.0 
+             PATHS ${_LibGLIB2LinkDir} )
 
-if(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
-   set(GLIB2_FOUND TRUE)
-endif(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GLIB2  DEFAULT_MSG  GLIB2_LIBRARIES GLIB2_INCLUDE_DIR)
 
-if(GLIB2_FOUND)
-   if(NOT GLIB2_FIND_QUIETLY)
-      message(STATUS "Found glib2: ${GLIB2_LIBRARIES} ${GLIB2_LIBRARIES}")
-   endif(NOT GLIB2_FIND_QUIETLY)
-else(GLIB2_FOUND)
-   if (GLIB2_FIND_REQUIRED)
-      message(FATAL_ERROR "Could NOT find glib2 library")
-   endif (GLIB2_FIND_REQUIRED)
-endif(GLIB2_FOUND)
-
-MARK_AS_ADVANCED(GLIB2_INCLUDE_DIR GLIB2_LIBRARIES)
+mark_as_advanced(GLIB2_INCLUDE_DIR GLIB2_LIBRARIES)
