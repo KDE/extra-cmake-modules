c6204d8966e3359915185d5f5b8018db1b6857a5
Adriaan de Groot <groot@kde.org>
2007-08-20 15:00:00 +0000

    Not all krb5 implementations have GSSAPI; the krb5-config script then returns an error message which isn't a useful library name
    svn path=/trunk/KDE/kdelibs/; revision=702399

--- modules/FindGSSAPI.cmake
+++ modules/FindGSSAPI.cmake
@@ -30,7 +30,12 @@ ELSE(GSSAPI_LIBS AND GSSAPI_FLAVOR)
   
   IF(KRB5_CONFIG)
   
+    SET(HAVE_KRB5_GSSAPI TRUE)
     EXEC_PROGRAM(${KRB5_CONFIG} ARGS --libs gssapi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GSSAPI_LIBS)
+    IF(_return_VALUE)
+      MESSAGE(STATUS "GSSAPI configure check failed.")
+      SET(HAVE_KRB5_GSSAPI FALSE)
+    ENDIF(_return_VALUE)
   
     EXEC_PROGRAM(${KRB5_CONFIG} ARGS --cflags gssapi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GSSAPI_INCS)
     string(REGEX REPLACE "(\r?\n)+$" "" GSSAPI_INCS "${GSSAPI_INCS}")
@@ -44,6 +49,16 @@ ELSE(GSSAPI_LIBS AND GSSAPI_FLAVOR)
       SET(GSSAPI_FLAVOR "HEIMDAL")
     ENDIF(gssapi_flavor_tmp MATCHES ".*Massachusetts.*")
   
+    IF(NOT HAVE_KRB5_GSSAPI)
+      IF (gssapi_flavor_tmp MATCHES "Sun Microsystems.*")
+	MESSAGE(STATUS "Solaris Kerberos does not have GSSAPI; this is normal.")
+	SET(GSSAPI_LIBS)
+	SET(GSSAPI_INCS)
+      ELSE(gssapi_flavor_tmp MATCHES "Sun Microsystems.*")
+	MESSAGE(WARNING "${KRB5_CONFIG} failed unexpectedly.")
+      ENDIF(gssapi_flavor_tmp MATCHES "Sun Microsystems.*")
+    ENDIF(NOT HAVE_KRB5_GSSAPI)
+
     IF(GSSAPI_LIBS) # GSSAPI_INCS can be also empty, so don't rely on that
       SET(GSSAPI_FOUND TRUE)
       message(STATUS "Found GSSAPI: ${GSSAPI_LIBS}")
