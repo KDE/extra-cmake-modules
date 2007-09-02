bf3c9f9a492d63d82368fc46fbc11bb0f3a3eb93
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-09-02 16:43:55 +0000

    additional library name libpng12
    svn path=/trunk/KDE/kdelibs/; revision=707676

--- modules/FindPNG.cmake
+++ modules/FindPNG.cmake
@@ -24,7 +24,7 @@ IF(ZLIB_FOUND)
   /usr/local/include/libpng             # OpenBSD
   )
 
-  SET(PNG_NAMES ${PNG_NAMES} png libpng)
+  SET(PNG_NAMES ${PNG_NAMES} png libpng png12 libpng12)
   FIND_LIBRARY(PNG_LIBRARY
     NAMES ${PNG_NAMES}
   )
