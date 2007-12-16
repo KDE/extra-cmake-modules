0279149fee4ccd9233e1cf7b649f65e38faccaa3
Allen Winter <winter@kde.org>
2007-12-16 19:28:40 +0000

    complete rewrite.
    svn path=/trunk/KDE/kdelibs/; revision=749189

--- modules/FindXmms.cmake
+++ modules/FindXmms.cmake
@@ -1,41 +1,39 @@
 # Search xmms
+## Once done this will define
+#
+#  XMMS_FOUND       - system has xmms
+#  XMMS_INCLUDE_DIR - the xmms include directory
+#  XMMS_LIBRARIES   - Link these to use xmms
 #
 # Copyright (c) 2006, 2007 Laurent Montel, <montel@kde.org>
+# Copyright (c) 2007 Allen Winter <winter@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+if (XMMS_INCLUDE_DIR AND XMMS_LIBRARIES)
+  # in cache already
+  SET(XMMS_FOUND TRUE)
 
-OPTION(KDE4_XMMS  "enable support for XMMS")
+else (XMMS_INCLUDE_DIR AND XMMS_LIBRARIES)
+  IF (NOT WIN32)
+    # use pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    INCLUDE(UsePkgConfig)
 
-if(KDE4_XMMS)
-	FIND_PROGRAM(XMMSCONFIG_EXECUTABLE NAMES xmms-config
-	)
-	#reset vars
-	set(XMMS_LIBRARIES)
-	set(XMMS_INCLUDE_DIR)
+    PKGCONFIG(xmms _XMMSIncDir _XMMSLinkDir _XMMSLinkFlags _XMMSCflags)
+  ENDIF(NOT WIN32)
 
-	if(XMMSCONFIG_EXECUTABLE)
-  		EXEC_PROGRAM(${XMMSCONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE XMMS_LIBRARIES)
+  FIND_PATH(XMMS_INCLUDE_DIR xmmsctrl.h
+    PATHS ${_XMMSIncDir} PATH_SUFFIXES xmms)
 
-  		EXEC_PROGRAM(${XMMSCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE XMMS_INCLUDE_DIR)
-		set(XMMS_DEFINITIONS ${XMMS_INCLUDE_DIR})
-  	
-		IF(XMMS_LIBRARIES AND XMMS_INCLUDE_DIR)
-    			SET(XMMS_FOUND TRUE)
-		ENDIF(XMMS_LIBRARIES AND XMMS_INCLUDE_DIR)
+  FIND_LIBRARY(XMMS_LIBRARIES NAMES xmms
+    PATHS ${_XMMSLinkDir})
 
-	MARK_AS_ADVANCED(XMMS_INCLUDE_DIR XMMS_LIBRARIES)
-	endif(XMMSCONFIG_EXECUTABLE)
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xmms DEFAULT_MSG
+                                    XMMS_LIBRARIES XMMS_INCLUDE_DIR)
 
-  	if (XMMS_FOUND)
-    		if (NOT Xmms_FIND_QUIETLY)
-      			message(STATUS "Found Xmms: ${XMMS_LIBRARIES}")
-    		endif (NOT Xmms_FIND_QUIETLY)
-  	else (XMMS_FOUND)
-    		if (Xmms_FIND_REQUIRED)
-      			message(FATAL_ERROR "Could NOT find Xmms")
-    		endif (Xmms_FIND_REQUIRED)
-  	endif (XMMS_FOUND)
-endif(KDE4_XMMS)
+  MARK_AS_ADVANCED(XMMS_INCLUDE_DIR XMMS_LIBRARIES)
 
+endif (XMMS_INCLUDE_DIR AND XMMS_LIBRARIES)
