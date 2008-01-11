96fa4f5b130490766a58e80d0bfb38813c79efba
Josef Spillner <spillner@kde.org>
2008-01-11 18:43:57 +0000

    - libstreams is not enough, streamanalyzer is also needed (those two are separate packages sometimes)
    svn path=/trunk/KDE/kdelibs/; revision=760011

--- modules/FindStrigi.cmake
+++ modules/FindStrigi.cmake
@@ -115,7 +115,7 @@ endif (NOT WIN32 AND NOT HAVE_STRIGI_VERSION)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Strigi  
-                                  "Couldn't find Strigi streams library. Set the environment variable STRIGI_HOME (or CMAKE_FIND_PREFIX_PATH if using CMake >=2.5) to the strigi install dir."  
+                                  "Couldn't find Strigi streams and streamanalyzer libraries. Set the environment variable STRIGI_HOME (or CMAKE_FIND_PREFIX_PATH if using CMake >=2.5) to the strigi install dir."  
                                   STRIGI_STREAMS_LIBRARY  STRIGI_STREAMANALYZER_LIBRARY  STRIGI_INCLUDE_DIR)
 
 mark_as_advanced(STRIGI_INCLUDE_DIR STRIGI_STREAMANALYZER_LIBRARY STRIGI_STREAMS_LIBRARY STRIGI_STRIGIQTDBUSCLIENT_LIBRARY )
