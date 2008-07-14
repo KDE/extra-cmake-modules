c4dde17dd66a6deef414d84468f36d4dbebd9905
Allen Winter <winter@kde.org>
2008-07-14 12:09:46 +0000

    increase GENERIC library version from .1 to .2. DO NOT EVEN THINK ABOUT BACKPORTING THIS
    svn path=/trunk/KDE/kdelibs/; revision=832335

--- modules/KDE4Defaults.cmake
+++ modules/KDE4Defaults.cmake
@@ -29,10 +29,10 @@ endif (NOT CMAKE_SKIP_RPATH)
 
 # define the generic version of the libraries here
 # this makes it easy to advance it when the next KDE release comes
-set(GENERIC_LIB_VERSION "4.1.0")
+set(GENERIC_LIB_VERSION "4.2.0")
 set(GENERIC_LIB_SOVERSION "4")
 
-set(KDE_NON_GENERIC_LIB_VERSION "5.1.0")
+set(KDE_NON_GENERIC_LIB_VERSION "5.2.0")
 set(KDE_NON_GENERIC_LIB_SOVERSION "5")
 
 # windows does not support LD_LIBRARY_PATH or similar
