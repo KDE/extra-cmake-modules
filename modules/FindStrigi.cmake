47bb08384c0db1379ee144628bc7be283840cd29
Helio Castro <helio@kde.org>
2008-11-11 15:48:19 +0000

    Embraceing new cmake, another module moving to pkg_check_modules
    svn path=/trunk/KDE/kdelibs/; revision=882904

--- modules/FindStrigi.cmake
+++ modules/FindStrigi.cmake
@@ -1,76 +1,32 @@
 # - Try to find Strigi, a fast and small desktop search program (http://strigi.sourceforge.net )
-# Once done this will define
-#
-#  STRIGI_FOUND - system has Strigi
-#  STRIGI_INCLUDE_DIR - the Strigi include directory
-#  STRIGI_STREAMANALYZER_LIBRARY - Link these to use Strigi streamanalyzer
-#  STRIGI_STREAMS_LIBRARY - Link these to use Strigi streams
-#  STRIGI_LINE_ANALYZER_PREFIX - strigi plugin prefix
-#  STRIGI_THROUGH_ANALYZER_PREFIX - strigi plugin prefix
-
-# at first search only in the specified directories (NO_DEFAULT_PATH)
-# only if it wasn't found there, the second call to FIND_PATH/LIBRARY() 
-# will actually do something, Alex
 
 include(FindLibraryWithDebug)
 include(MacroPushRequiredVars)
 
 if(NOT STRIGI_MIN_VERSION)
-  set(STRIGI_MIN_VERSION "0.5.9")
+    set(STRIGI_MIN_VERSION "0.5.9")
 endif(NOT STRIGI_MIN_VERSION)
 
 if (NOT WIN32)
-  include(UsePkgConfig)
-  pkgconfig(libstreamanalyzer _STRIGI_INCLUDEDIR _STRIGI_LIBDIR _dummyLinkFlags _dummyCflags)
-  #message(STATUS "_STRIGI_INCLUDEDIR=${_STRIGI_INCLUDEDIR} _STRIGI_LIBDIR=${_STRIGI_LIBDIR}")
-
-  if(_dummyLinkFlags)
-
-    exec_program(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=${STRIGI_MIN_VERSION}
-                 libstreamanalyzer RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
-
-    if(NOT _return_VALUE STREQUAL "0")
-      message(STATUS "pkg-config query failed. did you set $PKG_CONFIG_PATH to the directory where strigi libstreamanalyzer.pc is installed?")
-      message(FATAL_ERROR "Didn't find strigi >= ${STRIGI_MIN_VERSION}")
-    else(NOT _return_VALUE STREQUAL "0")
-      if(NOT Strigi_FIND_QUIETLY)
-        message(STATUS "Found Strigi ${_strigiVersion}")
-      endif(NOT Strigi_FIND_QUIETLY)
-    endif(NOT _return_VALUE STREQUAL "0")
-  else(_dummyLinkFlags)
-    message(STATUS "pkgconfig didn't find strigi, couldn't check strigi version")
-  endif(_dummyLinkFlags)
+    find_package(PkgConfig)
+    pkg_check_modules(STRIGI libstreamanalyzer>=${STRIGI_MIN_VERSION})
 endif(NOT WIN32)
 
 if (WIN32)
-  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _program_FILES_DIR)
+    file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _program_FILES_DIR)
 endif(WIN32)
 
 file(TO_CMAKE_PATH "$ENV{STRIGI_HOME}" strigi_home)
-
-find_path(STRIGI_INCLUDE_DIR strigi/streamanalyzer.h
-  PATHS
-  ${strigi_home}/include
-  ${_STRIGI_INCLUDEDIR}
-  ${_program_FILES_DIR}/strigi/include
-  NO_DEFAULT_PATH
-)
-
-find_path(STRIGI_INCLUDE_DIR strigi/streamanalyzer.h)
 
 find_library_with_debug(STRIGI_STREAMANALYZER_LIBRARY
   WIN32_DEBUG_POSTFIX d
   NAMES streamanalyzer
   PATHS
   ${strigi_home}/lib
-  ${_STRIGI_LIBDIR}
+  ${STRIGI_LIBRARY_DIRS}
   ${_program_FILES_DIR}/strigi/lib
   NO_DEFAULT_PATH
 )
-
-find_library_with_debug(STRIGI_STREAMANALYZER_LIBRARY  
-                        WIN32_DEBUG_POSTFIX d  
-                        NAMES streamanalyzer )
 
 
 find_library_with_debug(STRIGI_STREAMS_LIBRARY
@@ -78,36 +34,22 @@ find_library_with_debug(STRIGI_STREAMS_LIBRARY
   NAMES streams
   PATHS
   ${strigi_home}/lib
-  ${_STRIGI_LIBDIR}
+  ${STRIGI_LIBRARY_DIRS}
   ${_program_FILES_DIR}/strigi/lib
   NO_DEFAULT_PATH
 )
-
-find_library_with_debug(STRIGI_STREAMS_LIBRARY
-                        WIN32_DEBUG_POSTFIX d
-                        NAMES streams )
 
 find_library_with_debug(STRIGI_STRIGIQTDBUSCLIENT_LIBRARY
   WIN32_DEBUG_POSTFIX d
   NAMES strigiqtdbusclient
   PATHS
   ${strigi_home}/lib
-  ${_STRIGI_LIBDIR}
+  ${STRIGI_LIBRARY_DIRS}
   ${_program_FILES_DIR}/strigi/lib
   NO_DEFAULT_PATH
 )
 
-find_library_with_debug(STRIGI_STRIGIQTDBUSCLIENT_LIBRARY
-                        WIN32_DEBUG_POSTFIX d
-                        NAMES strigiqtdbusclient )
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Strigi  
-    "Couldn't find Strigi streams and streamanalyzer libraries. Set the environment variable STRIGI_HOME (or CMAKE_PREFIX_PATH if using CMake >=2.6) to the strigi install dir."  
-    STRIGI_STREAMS_LIBRARY  STRIGI_STREAMANALYZER_LIBRARY  STRIGI_INCLUDE_DIR)
-
 if (STRIGI_FOUND)
-
     # Check for the SIC change between 0.5.9 and 0.6.0...
 
     MACRO(MACRO_CHECK_STRIGI_API_SCREWUP _RETTYPE _RESULT)
@@ -134,7 +76,7 @@ int main()
 
     INCLUDE(CheckCXXSourceCompiles)
     macro_push_required_vars()
-    set( CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${STRIGI_INCLUDE_DIR} )
+    set( CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${STRIGI_INCLUDEDIR} )
     set( CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${STRIGI_STREAMS_LIBRARY} ${STRIGI_STREAMANALYZER_LIBRARY} )
     MACRO_CHECK_STRIGI_API_SCREWUP( "signed char" STRIGI_NEEDS_SIGNED_CHAR )
     MACRO_CHECK_STRIGI_API_SCREWUP( "char" STRIGI_NEEDS_CHAR )
@@ -179,7 +121,7 @@ else(WIN32)
   set(STRIGI_THROUGH_ANALYZER_PREFIX strigita_)
 endif(WIN32)
 
-mark_as_advanced(STRIGI_INCLUDE_DIR STRIGI_STREAMANALYZER_LIBRARY STRIGI_STREAMS_LIBRARY STRIGI_STRIGIQTDBUSCLIENT_LIBRARY 
+mark_as_advanced(STRIGI_STREAMANALYZER_LIBRARY STRIGI_STREAMS_LIBRARY STRIGI_STRIGIQTDBUSCLIENT_LIBRARY 
    STRIGI_LINE_ANALYZER_PREFIX
    STRIGI_THROUGH_ANALYZER_PREFIX
    STRIGI_NEEDS_SIGNED_CHAR
