9b429eb292024c8c5eafb5d052606f0661af7662
Alexander Neundorf <neundorf@kde.org>
2008-11-14 18:17:03 +0000

    Revert those files back to rev 882880 to get them into a known working state again, except FindStrigi.cmake, which seems to be working. Let's start now cleaning them up step by step.
    Alex
    
    CCMAIL: helio@kde.org
    
    
    svn path=/trunk/KDE/kdelibs/; revision=884355

--- modules/FindKexiv2.cmake
+++ modules/FindKexiv2.cmake
@@ -1,41 +1,103 @@
-# - Try to find the Kexiv2 library using cmake pkg_check_modulesi otherwise fallback to standard search
-# Once done this will define:
+# - Try to find the KExiv2 library
+# Once done this will define
 #
 #  KEXIV2_FOUND - system has libkexiv2
-#  KEXIV2_INCLUDEDIR - the libkexiv2 include directory
+#  KEXIV2_INCLUDE_DIR - the libkexiv2 include directory
 #  KEXIV2_LIBRARIES - Link these to use libkexiv2
 #  KEXIV2_DEFINITIONS - Compiler switches required for using libkexiv2
+#
 
+if (KEXIV2_INCLUDE_DIR AND KEXIV2_LIBRARIES)
 
-if (KEXIV2_INCLUDEDIR AND KEXIV2_LIBRARIES)
-    # in cache already
-    SET(KEXIV2_FOUND TRUE)
-else (KEXIV2_INCLUDEDIR AND KEXIV2_LIBRARIES)
-    if(NOT WIN32)
-        find_package(PkgConfig)
-        if(PKG_CONFIG_EXECUTABLE)
-            pkg_check_modules(KEXIV2 libkexiv2>=0.2.0)
-        endif(PKG_CONFIG_EXECUTABLE)
+  message(STATUS "Found Kexiv2 library in cache: ${KEXIV2_LIBRARIES}")
+
+  # in cache already
+  SET(KEXIV2_FOUND TRUE)
+
+else (KEXIV2_INCLUDE_DIR AND KEXIV2_LIBRARIES)
+
+  message(STATUS "Check Kexiv2 library in local sub-folder...")
+
+  # Check if library is not in local sub-folder
+
+  FIND_FILE(KEXIV2_LOCAL_FOUND libkexiv2/version.h.cmake ${CMAKE_SOURCE_DIR}/libkexiv2 ${CMAKE_SOURCE_DIR}/libs/libkexiv2 NO_DEFAULT_PATH)
+
+  if (KEXIV2_LOCAL_FOUND)
+
+    FIND_FILE(KEXIV2_LOCAL_FOUND_IN_LIBS libkexiv2/version.h.cmake ${CMAKE_SOURCE_DIR}/libs/libkexiv2 NO_DEFAULT_PATH)
+    if (KEXIV2_LOCAL_FOUND_IN_LIBS)
+      set(KEXIV2_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libs/libkexiv2)
+    else (KEXIV2_LOCAL_FOUND_IN_LIBS)
+      set(KEXIV2_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libkexiv2)
+    endif (KEXIV2_LOCAL_FOUND_IN_LIBS)
+    set(KEXIV2_DEFINITIONS "-I${KEXIV2_INCLUDE_DIR}")
+    set(KEXIV2_LIBRARIES kexiv2)
+    message(STATUS "Found Kexiv2 library in local sub-folder: ${KEXIV2_INCLUDE_DIR}")
+    set(KEXIV2_FOUND TRUE)
+    MARK_AS_ADVANCED(KEXIV2_INCLUDE_DIR KEXIV2_LIBRARIES)
+
+  else(KEXIV2_LOCAL_FOUND)
+    if(NOT WIN32) 
+      message(STATUS "Check Kexiv2 library using pkg-config...")
+
+      # use pkg-config to get the directories and then use these values
+      # in the FIND_PATH() and FIND_LIBRARY() calls
+      INCLUDE(UsePkgConfig)
+    
+      PKGCONFIG(libkexiv2 _KEXIV2IncDir _KEXIV2LinkDir _KEXIV2LinkFlags _KEXIV2Cflags)
+    
+      if(_KEXIV2LinkFlags)
+        # query pkg-config asking for a libkexiv2 >= 0.2.0
+        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.2.0 libkexiv2 RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+        if(_return_VALUE STREQUAL "0")
+            message(STATUS "Found libkexiv2 release >= 0.2.0")
+            set(KEXIV2_VERSION_GOOD_FOUND TRUE)
+        else(_return_VALUE STREQUAL "0")
+            message(STATUS "Found libkexiv2 release < 0.2.0, too old")
+            set(KEXIV2_VERSION_GOOD_FOUND FALSE)
+            set(KEXIV2_FOUND FALSE)
+        endif(_return_VALUE STREQUAL "0")
+      else(_KEXIV2LinkFlags)
+        set(KEXIV2_VERSION_GOOD_FOUND FALSE)
+        set(KEXIV2_FOUND FALSE)
+      endif(_KEXIV2LinkFlags)
+    else(NOT WIN32)
+      set(KEXIV2_VERSION_GOOD_FOUND TRUE)
     endif(NOT WIN32)
 
-    if(NOT KEXIV2_FOUND)
-        find_path(KEXIV2_INCLUDEDIR libkexiv2/version.h)
-        find_library(KEXIV2_LIBRARIES NAMES kexiv2)
-        if (KEXIV2_INCLUDEDIR AND KEXIV2_LIBRARIES)
-            set(KEXIV2_FOUND TRUE)
-        endif (KEXIV2_INCLUDEDIR AND KEXIV2_LIBRARIES)
-    endif(NOT KEXIV2_FOUND)
+    if(KEXIV2_VERSION_GOOD_FOUND)
+        set(KEXIV2_DEFINITIONS "${_KEXIV2Cflags}")
     
-    if(KEXIV2_FOUND)
-        set(KEXIV2_DEFINITIONS ${KEXIV2_CFLAGS})
-        if (NOT Kexiv2_FIND_QUIETLY)
-            message(STATUS "Found libkexiv2: ${KEXIV2_LIBRARIES}")
-        endif (NOT Kexiv2_FIND_QUIETLY)
-        set(KEXIV2_INCLUDE_DIR ${KEXIV2_INCLUDEDIR})
-        mark_as_advanced( KEXIV2_INCLUDE_DIR )
-    else(KEXIV2_FOUND)
-        if (Kexiv2_FIND_REQUIRED)
-            message(FATAL_ERROR "Not found required libkexiv2")
-        endif (Kexiv2_FIND_REQUIRED)
-    endif (KEXIV2_FOUND)
-endif (KEXIV2_INCLUDEDIR AND KEXIV2_LIBRARIES)
+        FIND_PATH(KEXIV2_INCLUDE_DIR libkexiv2/version.h
+        ${_KEXIV2IncDir}
+        )
+    
+        FIND_LIBRARY(KEXIV2_LIBRARIES NAMES kexiv2
+        PATHS
+        ${_KEXIV2LinkDir}
+        )
+    
+        if (KEXIV2_INCLUDE_DIR AND KEXIV2_LIBRARIES)
+            set(KEXIV2_FOUND TRUE)
+        endif (KEXIV2_INCLUDE_DIR AND KEXIV2_LIBRARIES)
+      endif(KEXIV2_VERSION_GOOD_FOUND) 
+      if (KEXIV2_FOUND)
+          if (NOT Kexiv2_FIND_QUIETLY)
+              message(STATUS "Found libkexiv2: ${KEXIV2_LIBRARIES}")
+          endif (NOT Kexiv2_FIND_QUIETLY)
+      else (KEXIV2_FOUND)
+          if (Kexiv2_FIND_REQUIRED)
+              if (NOT KEXIV2_INCLUDE_DIR)
+                  message(FATAL_ERROR "Could NOT find libkexiv2 header files")
+              endif (NOT KEXIV2_INCLUDE_DIR)
+              if (NOT KEXIV2_LIBRARIES)
+                  message(FATAL_ERROR "Could NOT find libkexiv2 library")
+              endif (NOT KEXIV2_LIBRARIES)
+          endif (Kexiv2_FIND_REQUIRED)
+      endif (KEXIV2_FOUND)
+    
+    MARK_AS_ADVANCED(KEXIV2_INCLUDE_DIR KEXIV2_LIBRARIES)
+
+  endif(KEXIV2_LOCAL_FOUND)
+  
+endif (KEXIV2_INCLUDE_DIR AND KEXIV2_LIBRARIES)
--- modules/FindKipi.cmake
+++ modules/FindKipi.cmake
@@ -1,41 +1,97 @@
-# - Try to find the Kipi library using cmake pkg_check_modulesi otherwise fallback to standard search
-# Once done this will define:
+# - Try to find the Kipi library
+# Once done this will define
 #
 #  KIPI_FOUND - system has libkipi
-#  KIPI_INCLUDEDIR - the libkipi include directory
+#  KIPI_INCLUDE_DIR - the libkipi include directory
 #  KIPI_LIBRARIES - Link these to use libkipi
 #  KIPI_DEFINITIONS - Compiler switches required for using libkipi
+#
 
+if (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
 
-if (KIPI_INCLUDEDIR AND KIPI_LIBRARIES)
-    # in cache already
-    SET(KIPI_FOUND TRUE)
-else (KIPI_INCLUDEDIR AND KIPI_LIBRARIES)
+  message(STATUS "Found Kipi library in cache: ${KIPI_LIBRARIES}")
+
+  # in cache already
+  SET(KIPI_FOUND TRUE)
+
+else (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+
+  message(STATUS "Check Kipi library in local sub-folder...")
+
+  # Check if library is not in local sub-folder
+
+  find_file (KIPI_LOCAL_FOUND libkipi/version.h.cmake ${CMAKE_SOURCE_DIR}/libkipi ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
+
+  if (KIPI_LOCAL_FOUND)
+
+    find_file (KIPI_LOCAL_FOUND_IN_LIBS libkipi/version.h.cmake ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
+    if (KIPI_LOCAL_FOUND_IN_LIBS)
+      set(KIPI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libs/libkipi)
+    else (KIPI_LOCAL_FOUND_IN_LIBS)
+      set(KIPI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libkipi)
+    endif (KIPI_LOCAL_FOUND_IN_LIBS)
+    set(KIPI_DEFINITIONS -I${KIPI_INCLUDE_DIR})
+    set(KIPI_LIBRARIES kipi)
+    message(STATUS "Found Kipi library in local sub-folder: ${KIPI_INCLUDE_DIR}")
+    set(KIPI_FOUND TRUE)
+    mark_as_advanced(KIPI_INCLUDE_DIR KIPI_LIBRARIES)
+
+  else(KIPI_LOCAL_FOUND)
+
     if(NOT WIN32)
-        find_package(PkgConfig)
-        if(PKG_CONFIG_EXECUTABLE)
-            pkg_check_modules(KIPI libkipi>=0.2.0)
-        endif(PKG_CONFIG_EXECUTABLE)
-    endif(NOT WIN32)
+      message(STATUS "Check Kipi library using pkg-config...")
 
-    if(NOT KIPI_FOUND)
-        find_path(KIPI_INCLUDEDIR libkipi/version.h)
-        find_library(KIPI_LIBRARIES NAMES kipi)
-        if (KIPI_INCLUDEDIR AND KIPI_LIBRARIES)
+      # use pkg-config to get the directories and then use these values
+      # in the FIND_PATH() and FIND_LIBRARY() calls
+      INCLUDE(UsePkgConfig)
+
+      PKGCONFIG(libkipi _KIPIIncDir _KIPILinkDir _KIPILinkFlags _KIPICflags)
+
+      if(_KIPILinkFlags)
+        # query pkg-config asking for a libkipi >= 0.2.0
+        exec_program(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.2.0 libkipi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+        if(_return_VALUE STREQUAL "0")
+            message(STATUS "Found libkipi release >= 0.2.0")
+            set(KIPI_VERSION_GOOD_FOUND TRUE)
+        else(_return_VALUE STREQUAL "0")
+            message(STATUS "Found libkipi release < 0.2.0, too old")
+            set(KIPI_VERSION_GOOD_FOUND FALSE)
+            set(KIPI_FOUND FALSE)
+        endif(_return_VALUE STREQUAL "0")
+      else(_KIPILinkFlags)
+        set(KIPI_VERSION_GOOD_FOUND FALSE)
+        set(KIPI_FOUND FALSE)
+      endif(_KIPILinkFlags)
+    else(NOT WIN32)
+      set(KIPI_VERSION_GOOD_FOUND TRUE)
+    endif(NOT WIN32)
+    if(KIPI_VERSION_GOOD_FOUND)
+        set(KIPI_DEFINITIONS ${_KIPICflags})
+
+        find_path(KIPI_INCLUDE_DIR NAMES libkipi/version.h PATHS ${KDE4_INCLUDE_DIR} ${_KIPIIncDir})
+        find_library(KIPI_LIBRARIES NAMES kipi PATHS ${KDE4_LIB_DIR} ${_KIPILinkDir})
+
+        if (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
             set(KIPI_FOUND TRUE)
-        endif (KIPI_INCLUDEDIR AND KIPI_LIBRARIES)
-    endif(NOT KIPI_FOUND)
+        endif (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+      endif(KIPI_VERSION_GOOD_FOUND)
+      if (KIPI_FOUND)
+          if (NOT Kipi_FIND_QUIETLY)
+              message(STATUS "Found libkipi: ${KIPI_LIBRARIES}")
+          endif (NOT Kipi_FIND_QUIETLY)
+      else (KIPI_FOUND)
+          if (Kipi_FIND_REQUIRED)
+              if (NOT KIPI_INCLUDE_DIR)
+                  message(FATAL_ERROR "Could NOT find libkipi header files")
+              endif (NOT KIPI_INCLUDE_DIR)
+              if (NOT KIPI_LIBRARIES)
+                  message(FATAL_ERROR "Could NOT find libkipi library")
+              endif (NOT KIPI_LIBRARIES)
+          endif (Kipi_FIND_REQUIRED)
+      endif (KIPI_FOUND)
     
-    if(KIPI_FOUND)
-        set(KIPI_DEFINITIONS ${KIPI_CFLAGS})
-        if (NOT Kipi_FIND_QUIETLY)
-            message(STATUS "Found libkipi: ${KIPI_LIBRARIES}")
-        endif (NOT Kipi_FIND_QUIETLY)
-        set(KIPI_INCLUDE_DIR ${KIPI_INCLUDEDIR})
-        mark_as_advanced( KIPI_INCLUDE_DIR )
-    else(KIPI_FOUND)
-        if (Kipi_FIND_REQUIRED)
-            message(FATAL_ERROR "Not found required libkipi")
-        endif (Kipi_FIND_REQUIRED)
-    endif (KIPI_FOUND)
-endif (KIPI_INCLUDEDIR AND KIPI_LIBRARIES)
+    MARK_AS_ADVANCED(KIPI_INCLUDE_DIR KIPI_LIBRARIES)
+
+  endif(KIPI_LOCAL_FOUND)
+  
+endif (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
