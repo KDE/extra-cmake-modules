5020bf83969392d03b7bdbd775a9b42fdf2a6b49
Marcus Hufgard <marcus.hufgard@hufgard.de>
2009-01-14 14:26:33 +0000

    we need the PulseAudio Mainloop Library in kdemultimedia.
    Please have a look over it and expand it to win32 libs.
    
    Thanks
    
    svn path=/trunk/KDE/kdelibs/; revision=910978

--- modules/FindPulseAudio.cmake
+++ modules/FindPulseAudio.cmake
@@ -5,6 +5,7 @@
 #  PULSEAUDIO_FOUND - system has the PulseAudio library
 #  PULSEAUDIO_INCLUDE_DIR - the PulseAudio include directory
 #  PULSEAUDIO_LIBRARY - the libraries needed to use PulseAudio
+#  PULSEAUDIO_MAINLOOP_LIBRARY - the libraries needed to use PulsAudio Mailoop
 #
 # Copyright (c) 2008, Matthias Kretz, <kretz@kde.org>
 #
@@ -15,10 +16,10 @@ if (NOT PULSEAUDIO_MINIMUM_VERSION)
   set(PULSEAUDIO_MINIMUM_VERSION "0.9.9")
 endif (NOT PULSEAUDIO_MINIMUM_VERSION)
 
-if (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
+if (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY AND PULSEAUDIO_MAINLOOP_LIBRARY)
    # Already in cache, be silent
    set(PULSEAUDIO_FIND_QUIETLY TRUE)
-endif (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
+endif (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY AND PULSEAUDIO_MAINLOOP_LIBRARY)
 
 if (NOT WIN32)
    include(FindPkgConfig)
@@ -28,6 +29,10 @@ if (NOT WIN32)
       set(PULSEAUDIO_INCLUDE_DIR ${PULSEAUDIO_INCLUDEDIR} CACHE PATH "Path to the PulseAudio includes")
       #  PULSEAUDIO_DEFINITIONS - Compiler switches required for using PulseAudio
       #  set(PULSEAUDIO_DEFINITIONS ${PULSEAUDIO_CFLAGS})
+      pkg_check_modules(PULSEAUDIO_MAINLOOP libpulse-mainloop-glib)
+      if(PULSEAUDIO_MAINLOOP_FOUND)
+          set(PULSEAUDIO_MAINLOOP_LIBRARY ${PULSEAUDIO_MAINLOOP_LIBRARIES} CACHE FILEPATH "Path to the PulsAudio Mainloop library")
+      endif(PULSEAUDIO_MAINLOOP_FOUND)
    endif(PULSEAUDIO_FOUND)
 else (NOT WIN32)
 
@@ -48,9 +53,14 @@ endif( NOT WIN32)
 if (PULSEAUDIO_FOUND)
    if (NOT PULSEAUDIO_FIND_QUIETLY)
       message(STATUS "Found PulseAudio: ${PULSEAUDIO_LIBRARY}")
+      if (PULSEAUDIO_MAINLOOP_FOUND)
+          message(STATUS "Found PulseAudio Mainloop: ${PULSEAUDIO_MAINLOOP_LIBRARY}")
+      else (PULSAUDIO_MAINLOOP_FOUND)
+          message(STATUS "Could NOT find PulseAudio Mainloop Library")
+      endif (PULSEAUDIO_MAINLOOP_FOUND)
    endif (NOT PULSEAUDIO_FIND_QUIETLY)
 else (PULSEAUDIO_FOUND)
    message(STATUS "Could NOT find PulseAudio")
 endif (PULSEAUDIO_FOUND)
 
-#mark_as_advanced(PULSEAUDIO_INCLUDE_DIR PULSEAUDIO_LIBRARY)
+#mark_as_advanced(PULSEAUDIO_INCLUDE_DIR PULSEAUDIO_LIBRARY PULSEAUDIO_MAINLOOP_LIBRARY)
