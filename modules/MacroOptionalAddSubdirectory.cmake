200901dce93b54946d79b6d38564cddf14990afe
Alexander Neundorf <neundorf@kde.org>
2009-03-17 21:27:31 +0000

    Add a cmake variable DISABLE_ALL_OPTIONAL_SUBDIRECTORIES which can be set to TRUE to disable all subdirectories by default This should make building project in a modular way easier.
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=940644

--- modules/MacroOptionalAddSubdirectory.cmake
+++ modules/MacroOptionalAddSubdirectory.cmake
@@ -9,6 +9,12 @@
 #     subdirectory.
 #     This is useful if you want to compile only a subset of all
 #     directories.
+#
+# If the CMake variable DISABLE_ALL_OPTIONAL_SUBDIRECTORIES is set to TRUE
+# for the first CMake run on the project, all subdirectories will be disabled
+# by default (but can of course be enabled via the respective options).
+# E.g. the following will disable all subdirectories except the one named "kcalc":
+#   $ cmake -DDISABLE_ALL_OPTIONAL_SUBDIRECTORIES=TRUE -DBUILD_kcalc=TRUE <srcdir>
 
 # Copyright (c) 2007, Alexander Neundorf, <neundorf@kde.org>
 #
@@ -19,7 +25,11 @@
 MACRO (MACRO_OPTIONAL_ADD_SUBDIRECTORY _dir )
    GET_FILENAME_COMPONENT(_fullPath ${_dir} ABSOLUTE)
    IF(EXISTS ${_fullPath})
-      SET(_DEFAULT_OPTION_VALUE TRUE)
+      IF(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
+         SET(_DEFAULT_OPTION_VALUE FALSE)
+      ELSE(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
+         SET(_DEFAULT_OPTION_VALUE TRUE)
+      ENDIF(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
       IF(DISABLE_ALL_OPTIONAL_SUBDIRS  AND NOT DEFINED  BUILD_${_dir})
          SET(_DEFAULT_OPTION_VALUE FALSE)
       ENDIF(DISABLE_ALL_OPTIONAL_SUBDIRS  AND NOT DEFINED  BUILD_${_dir})
