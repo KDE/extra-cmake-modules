8905501ac368562a62408b50768da681c0fd8bb2
Gökcen Eraslan <gokcen@pardus.org.tr>
2009-06-10 13:01:43 +0000

    Run chmod on compiled files, instead of files installed on system.
    CCMAIL: montel@kde.org
    
    
    svn path=/trunk/KDE/kdelibs/; revision=979688

--- modules/create_exe_symlink.cmake
+++ modules/create_exe_symlink.cmake
@@ -11,6 +11,6 @@ FILE(MAKE_DIRECTORY ${abs_link_path})
 GET_FILENAME_COMPONENT(abs_target ${TARGET} ABSOLUTE)
 IF(UNIX)
     EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E create_symlink ${abs_target} ${abs_link_name})
-    EXECUTE_PROCESS(COMMAND chmod a+x ${abs_target})
+    EXECUTE_PROCESS(COMMAND chmod a+x $ENV{DESTDIR}/${abs_target})
 ENDIF(UNIX)
 # FIXME: WIN32 support
