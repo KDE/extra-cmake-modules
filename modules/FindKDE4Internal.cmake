967c0cbe1f5dabd2825fc2bc42b14403458b7185
Carlo Segato <brandon.ml@gmail.com>
2009-08-09 22:30:38 +0000

    add CMAKE_C_COMPILER_ARG1 as argument when executing the compiler, this way cmake doesn't fail if we set CC to something like 'distcc /path/to/gcc'
    svn path=/trunk/KDE/kdelibs/; revision=1009414

--- modules/FindKDE4Internal.cmake
+++ modules/FindKDE4Internal.cmake
@@ -1054,7 +1054,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
    set( __KDE_HAVE_GCC_VISIBILITY ${__KDE_HAVE_GCC_VISIBILITY} CACHE BOOL "GCC support for hidden visibility")
 
    # get the gcc version
-   exec_program(${CMAKE_C_COMPILER} ARGS --version OUTPUT_VARIABLE _gcc_version_info)
+   exec_program(${CMAKE_C_COMPILER} ARGS ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info)
 
    string (REGEX MATCH "[345]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
    # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the patch level, handle this here:
@@ -1075,7 +1075,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
 
    set(_GCC_COMPILED_WITH_BAD_ALLOCATOR FALSE)
    if (GCC_IS_NEWER_THAN_4_1)
-      exec_program(${CMAKE_C_COMPILER} ARGS -v OUTPUT_VARIABLE _gcc_alloc_info)
+      exec_program(${CMAKE_C_COMPILER} ARGS ${CMAKE_C_COMPILER_ARG1} -v OUTPUT_VARIABLE _gcc_alloc_info)
       string(REGEX MATCH "(--enable-libstdcxx-allocator=mt)" _GCC_COMPILED_WITH_BAD_ALLOCATOR "${_gcc_alloc_info}")
    endif (GCC_IS_NEWER_THAN_4_1)
 
