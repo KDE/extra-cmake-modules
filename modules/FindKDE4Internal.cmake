c083a3799c48dedbef7be7f9eec463f5ecc3d927
Romain Pokrzywka <romain@kdab.net>
2009-10-22 11:09:05 +0000

    better place for the extra msvc compile flag
    svn path=/trunk/KDE/kdelibs/; revision=1038952

--- modules/FindKDE4Internal.cmake
+++ modules/FindKDE4Internal.cmake
@@ -883,9 +883,6 @@ if (WIN32)
    if(MSVC)
       set( _KDE4_PLATFORM_DEFINITIONS -DKDE_FULL_TEMPLATE_EXPORT_INSTANTIATION -DWIN32_LEAN_AND_MEAN )
       
-      # Qt disables the native wchar_t type, do it too to avoid linking issues
-      set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Zc:wchar_t-" )
-      
       # C4250: 'class1' : inherits 'class2::member' via dominance
       set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -wd4250" )
       # C4251: 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
@@ -912,7 +909,7 @@ if (WIN32)
    endif(MSVC_IDE)
 
    # we don't support anything below w2k and all winapi calls are unicodes
-   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN32_WINNT=0x0501 -D_WIN32_IE=0x0501 -DUNICODE" )
+   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN32_WINNT=0x0501 -DWINVER=0x0501 -D_WIN32_IE=0x0501 -DUNICODE" )
 endif (WIN32)
 
 
@@ -1042,6 +1039,9 @@ endmacro(KDE_CHECK_FLAG_EXISTS FLAG VAR)
 
 if (MSVC)
    set (KDE4_ENABLE_EXCEPTIONS -EHsc)
+
+   # Qt disables the native wchar_t type, do it too to avoid linking issues
+   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Zc:wchar_t-" )   
 
    # make sure that no header adds libcmt by default using #pragma comment(lib, "libcmt.lib") as done by mfc/afx.h
    kde_check_flag_exists("/NODEFAULTLIB:libcmt /DEFAULTLIB:msvcrt" CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "Release with Debug Info")
