e9a85eb38858566704041b761a5b8bfa3222e0e3
Alexander Neundorf <neundorf@kde.org>
2009-11-25 23:47:30 +0000

    Revert the new cmake module committed without any review or announcement and which was in a broken state.
    This has the effect that the condition for building nepomuk is now never true (instead of sometimes), but 
    I have the impression that for successfully building all of KDE nepomuk is always required, since the nepomuk/trig parsing is
    also used e.g. in kdepim and I think also in kdenetwork. 
    So the behaviour was already wrong, and now it is more exposed. This should help getting it fixed.
    
    Also, this commit makes explicit that soprano with redland backend and raptorparser are in fact required for building KDE since last week.
    
    Alex
    
    CCMAIL: trueg@kde.org
    CCMAIL: kde-buildsystem@kde.org
    CCMAIL: kde-core-devel@kde.org
    
    
    svn path=/trunk/KDE/kdelibs/; revision=1054396

--- modules/FindSharedDesktopOntologies.cmake
+++ modules/FindSharedDesktopOntologies.cmake
@@ -1,36 +1,4 @@
-# - Try to find shared-desktop-ontologies
-# Once done this will define
-#
-#  DESKTOP_ONTOLOGIES_FOUND - system has shared-desktop-ontologies
-#  DESKTOP_ONTOLOGIES_DIR - Folder where the ontologies are stored
-#  DESKTOP_ONTOLOGIES_VERSION - version number of shared-desktop-ontologies
+# This module was committed without any review or announcement and in a not acceptable state, so disable it for now. Alex
+message(STATUS "FindSharedDesktopOntologies.cmake is just a placeholder until a reviewed module is committed")
 
-# Copyright (c) 2009, Sebastian Trueg, <trueg@kde.org>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
-IF (DESKTOP_ONTOLOGIES_DIR)
-   # in cache already
-   SET(SharedDesktopOntologies_FIND_QUIETLY TRUE)
-ENDIF (DESKTOP_ONTOLOGIES_DIR)
-
-IF (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
-   find_package(PkgConfig)
-   PKG_SEARCH_MODULE( DESKTOP_ONTOLOGIES shared-desktop-ontologies )
-   set(DESKTOP_ONTOLOGIES_DIR ${DESKTOP_ONTOLOGIES_PREFIX}/share/ontology)
-ENDIF (NOT WIN32)
-
-IF (DESKTOP_ONTOLOGIES_FOUND)
-   IF (NOT SharedDesktopOntologies_FIND_QUIETLY)
-      MESSAGE(STATUS "Found SharedDesktopOntologies ${DESKTOP_ONTOLOGIES_VERSION}: ${DESKTOP_ONTOLOGIES_DIR}")
-   ENDIF (NOT SharedDesktopOntologies_FIND_QUIETLY)
-ELSE (DESKTOP_ONTOLOGIES_FOUND)
-   IF (SharedDesktopOntologies_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could NOT find SharedDesktopOntologies, check FindPkgConfig output above!")
-   ENDIF (SharedDesktopOntologies_FIND_REQUIRED)
-ENDIF (DESKTOP_ONTOLOGIES_FOUND)
-
-MARK_AS_ADVANCED(DESKTOP_ONTOLOGIES_DIR)
+set(DESKTOP_ONTOLOGIES_DIR "")
