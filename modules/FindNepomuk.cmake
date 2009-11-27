3ebe3e49632c59e189b309379ab5928029b3355a
Sebastian Trueg <sebastian@trueg.de>
2009-11-27 09:25:47 +0000

    Fixed Nepomuk build system including the shared ontologies. Approved by Andreas Pakulat.
    svn path=/trunk/KDE/kdelibs/; revision=1054973

--- modules/FindNepomuk.cmake
+++ modules/FindNepomuk.cmake
@@ -19,14 +19,14 @@
 if (NOT DEFINED Soprano_FOUND)
   find_package(Soprano)
   include(MacroLogFeature)
-  macro_log_feature(Soprano_FOUND "Soprano" "Semantic Desktop Storing" "" FALSE "" "Soprano is needed for Nepomuk")
+  macro_log_feature(Soprano_FOUND "Soprano" "Semantic Desktop Storing" "" TRUE "" "Soprano is needed for Nepomuk")
 endif (NOT DEFINED Soprano_FOUND)
 
-if (NOT DEFINED DESKTOP_ONTOLOGIES_FOUND)
+if (NOT DEFINED SHAREDDESKTOPONTOLOGIES_FOUND)
   find_package(SharedDesktopOntologies)
   include(MacroLogFeature)
-  macro_log_feature(DESKTOP_ONTOLOGIES_FOUND "Shared desktop ontologies" "Desktop ontologies" "http://oscaf.sourceforge.net" FALSE "" "Ontologies necessary for the Nepomuk semantic desktop.")
-endif (NOT DEFINED DESKTOP_ONTOLOGIES_FOUND)
+  macro_log_feature(SHAREDDESKTOPONTOLOGIES_FOUND "Shared desktop ontologies" "Desktop ontologies" "http://oscaf.sourceforge.net" TRUE "" "Ontologies necessary for the Nepomuk semantic desktop.")
+endif (NOT DEFINED SHAREDDESKTOPONTOLOGIES_FOUND)
 
 # Check for the following stuff independent from whether soprano has been found
 # or not. This will give a better error message at the end.
@@ -61,7 +61,7 @@ include(FindPackageHandleStandardArgs)
 # easier for the user to see what was missing:
 find_package_handle_standard_args(Nepomuk  DEFAULT_MSG 
                                   NEPOMUK_LIBRARIES NEPOMUK_INCLUDE_DIR
-                                  Soprano_FOUND SOPRANO_PLUGIN_RAPTORPARSER_FOUND SOPRANO_PLUGIN_REDLANDBACKEND_FOUND DESKTOP_ONTOLOGIES_FOUND
+                                  Soprano_FOUND SOPRANO_PLUGIN_RAPTORPARSER_FOUND SOPRANO_PLUGIN_REDLANDBACKEND_FOUND SHAREDDESKTOPONTOLOGIES_FOUND
                                   )
 
 #to retain backward compatibility
--- modules/FindSharedDesktopOntologies.cmake
+++ modules/FindSharedDesktopOntologies.cmake
@@ -1,4 +1,37 @@
-# This module was committed without any review or announcement and in a not acceptable state, so disable it for now. Alex
-message(STATUS "FindSharedDesktopOntologies.cmake is just a placeholder until a reviewed module is committed")
+# - Try to find shared-desktop-ontologies
+# The shared-desktop-ontologies package is a direct dependancy of the Nepomuk
+# semantic desktop system and provides all necessary ontology files like
+# RDF, RDFS, NRL, or NIE.
+#
+# The package is created by the OSCAF project (http://oscaf.sourceforge.net).
+#
+# Once done this will define
+#
+#  SHAREDDESKTOPONTOLOGIES_FOUND    - system has shared-desktop-ontologies
+#  SHAREDDESKTOPONTOLOGIES_ROOT_DIR - Folder where the ontologies are stored
+#
 
-set(DESKTOP_ONTOLOGIES_DIR "")
+# Copyright (c) 2009, Sebastian Trueg, <trueg@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+if (SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
+   # in cache already
+   set(SHAREDDESKTOPONTOLOGIES_FIND_QUIETLY TRUE)
+endif (SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
+
+find_path (SHAREDDESKTOPONTOLOGIES_ROOT_DIR
+  nie/nie.trig
+  PATHS
+  /usr/share
+  /usr/local/share
+  ${SHARE_INSTALL_PREFIX}
+  ENV XDG_DATA_DIRS
+  PATH_SUFFIXES ontology
+)
+
+mark_as_advanced(SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SharedDesktopOntologies DEFAULT_MSG SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
