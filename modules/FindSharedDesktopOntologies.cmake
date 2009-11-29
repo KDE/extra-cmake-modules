f9eb85764ac652b8afba04e363c67128a861a7e4
Sebastian Trueg <sebastian@trueg.de>
2009-11-29 21:27:26 +0000

    Support upcoming 0.2 version of the shared-desktop-ontologies which will bring a cmake config file
    svn path=/trunk/KDE/kdelibs/; revision=1056325

--- modules/FindSharedDesktopOntologies.cmake
+++ modules/FindSharedDesktopOntologies.cmake
@@ -7,8 +7,11 @@
 #
 # Once done this will define
 #
-#  SHAREDDESKTOPONTOLOGIES_FOUND    - system has shared-desktop-ontologies
-#  SHAREDDESKTOPONTOLOGIES_ROOT_DIR - Folder where the ontologies are stored
+#  SHAREDDESKTOPONTOLOGIES_FOUND         - system has shared-desktop-ontologies
+#  SHAREDDESKTOPONTOLOGIES_ROOT_DIR      - Folder where the ontologies are stored
+#  SHAREDDESKTOPONTOLOGIES_VERSION_MAJOR - The major version number, i.e. '1' in '1.2'
+#  SHAREDDESKTOPONTOLOGIES_VERSION_MINOR - The minor version number, i.e. '2' in '1.2'
+#  SHAREDDESKTOPONTOLOGIES_VERSION       - The complete version string, i.e. '1.2'
 #
 
 # Copyright (c) 2009, Sebastian Trueg, <trueg@kde.org>
@@ -21,20 +24,25 @@ if (SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
    set(SHAREDDESKTOPONTOLOGIES_FIND_QUIETLY TRUE)
 endif (SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
 
-# Look in the standard dirs
-find_path (SHAREDDESKTOPONTOLOGIES_ROOT_DIR
-  nie/nie.trig
-  PATHS
-  /usr/share
-  /usr/local/share
-  ${SHARE_INSTALL_PREFIX}
-  ENV XDG_DATA_DIRS
-  PATH_SUFFIXES ontology)
+# First try the SharedDesktopOntologiesConfig.cmake from shared-desktop-ontologies 0.2 and newer
+find_package(SharedDesktopOntologies ${SharedDesktopOntologies_FIND_VERSION} QUIET NO_MODULE)
 
-# Look in CMAKE_PREFIX_PATH
-find_path(SHAREDDESKTOPONTOLOGIES_ROOT_DIR
-  nie/nie.trig
-  PATH_SUFFIXES share/ontology)
+if (NOT SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
+  # Look in the standard dirs
+  find_path (SHAREDDESKTOPONTOLOGIES_ROOT_DIR
+    nie/nie.trig
+    PATHS
+    /usr/share
+    /usr/local/share
+    ${SHARE_INSTALL_PREFIX}
+    ENV XDG_DATA_DIRS
+    PATH_SUFFIXES ontology)
+
+  # Look in CMAKE_PREFIX_PATH
+  find_path(SHAREDDESKTOPONTOLOGIES_ROOT_DIR
+    nie/nie.trig
+    PATH_SUFFIXES share/ontology)
+endif (NOT SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
 
 mark_as_advanced(SHAREDDESKTOPONTOLOGIES_ROOT_DIR)
 
