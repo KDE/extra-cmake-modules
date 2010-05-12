5596ae8655380d7b8ec2548427f986f9939329ba
Kevin Ottens <ervin@kde.org>
2010-05-12 13:49:19 +0000

    Forgot to commit the new relevant cmake variables from the kutils split.
    svn path=/trunk/KDE/kdelibs/; revision=1125849

--- modules/FindKDE4Internal.cmake
+++ modules/FindKDE4Internal.cmake
@@ -50,6 +50,10 @@
 #  KDE4_KIO_LIBRARY         - the kio library
 #  KDE4_KPARTS_LIBRARY      - the kparts library
 #  KDE4_KUTILS_LIBRARY      - the kutils library
+#  KDE4_KEMOTICONS_LIBRARY  - the kemoticons library
+#  KDE4_KIDLETIME_LIBRARY   - the kidletime library
+#  KDE4_KCMUTILS_LIBRARY    - the kcmutils library
+#  KDE4_KPRINTUTILS_LIBRARY - the kprintutils library
 #  KDE4_KDE3SUPPORT_LIBRARY - the kde3support library
 #  KDE4_KFILE_LIBRARY       - the kfile library
 #  KDE4_KHTML_LIBRARY       - the khtml library
@@ -80,6 +84,10 @@
 #  KDE4_KIO_LIBS              - the kio library and all depending libraries
 #  KDE4_KPARTS_LIBS           - the kparts library and all depending libraries
 #  KDE4_KUTILS_LIBS           - the kutils library and all depending libraries
+#  KDE4_KEMOTICONS_LIBS       - the kemoticons library and all depending libraries
+#  KDE4_KIDLETIME_LIBS        - the kidletime library and all depending libraries
+#  KDE4_KCMUTILS_LIBS         - the kcmutils library and all depending libraries
+#  KDE4_KPRINTUTILS_LIBS      - the kprintutils library and all depending libraries
 #  KDE4_KDE3SUPPORT_LIBS      - the kde3support library and all depending libraries
 #  KDE4_KFILE_LIBS            - the kfile library and all depending libraries
 #  KDE4_KHTML_LIBS            - the khtml library and all depending libraries
@@ -612,13 +620,16 @@ endif (_kdeBootStrapping)
 # will be simply set to "kdecore".
 
 # Sorted by names:
+_kde4_set_lib_variables(KCMUTILS      kcmutils      "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KDE3SUPPORT   kde3support   "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KDECORE       kdecore       "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KDEUI         kdeui         "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KDEWEBKIT     kdewebkit     "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KDNSSD        kdnssd        "${KDE4_TARGET_PREFIX}")
+_kde4_set_lib_variables(KEMOTICONS    kemoticons    "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KFILE         kfile         "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KHTML         khtml         "${KDE4_TARGET_PREFIX}")
+_kde4_set_lib_variables(KIDLETIME     kidletime     "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KIO           kio           "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KJS           kjs           "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KJSAPI        kjsapi        "${KDE4_TARGET_PREFIX}")
@@ -626,6 +637,7 @@ _kde4_set_lib_variables(KNEWSTUFF2    knewstuff2    "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KNEWSTUFF3    knewstuff3    "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KNOTIFYCONFIG knotifyconfig "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KPARTS        kparts        "${KDE4_TARGET_PREFIX}")
+_kde4_set_lib_variables(KPRINTUTILS   kprintutils   "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KROSSCORE     krosscore     "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KROSSUI       krossui       "${KDE4_TARGET_PREFIX}")
 _kde4_set_lib_variables(KTEXTEDITOR   ktexteditor   "${KDE4_TARGET_PREFIX}")
