2dcad1910fd134e5e94255fa8924b469207556cf
Maciej Mrozowski <reavertm@gmail.com>
2010-06-30 01:35:19 +0000

    No need to force CMake colors anymore, see discussion http://lists.kde.org/?l=kde-buildsystem&m=127784710709842&w=2
    svn path=/trunk/KDE/kdelibs/; revision=1144480

--- modules/KDE4Defaults.cmake
+++ modules/KDE4Defaults.cmake
@@ -18,10 +18,6 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 # since cmake 2.4.1
 set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
 
-# Use colored output
-# since cmake 2.4.0
-set(CMAKE_COLOR_MAKEFILE ON)
-
 # define the generic version of the libraries here
 # this makes it easy to advance it when the next KDE release comes
 # Use this version number for libraries which are at version n in KDE version n
@@ -35,8 +31,8 @@ set(KDE_NON_GENERIC_LIB_SOVERSION "5")
 # windows does not support LD_LIBRARY_PATH or similar
 # all searchable directories has to be defined by the PATH environment var
 # to reduce the number of required pathes executables are placed into
-# the build bin dir 
+# the build bin dir
 if (WIN32)
  set (EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin)
 # set (LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin)
-endif(WIN32) 
+endif(WIN32)
