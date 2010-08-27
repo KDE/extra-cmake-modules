bb5e1ef1d1dad44afa7f2957eeef861a95a66202
Paulo Romulo <paulo.romulo@kdemail.net>
2010-08-27 07:16:09 +0000

    Building Solid UPnP backend by default now. HUPnP library required.
    svn path=/trunk/KDE/kdelibs/; revision=1168594

---
+++ modules/FindHUpnp.cmake
@@ -0,0 +1,18 @@
+# - Try to find HUPnP library
+#  Once done this will define
+#
+#  HUPNP_FOUND - system has HUPnP
+#  HUPNP_INCLUDE_DIR - the LIBHUpnp include directory
+#  HUPNP_LIBS - the LIBHUpnp libraries
+#
+# Copyright (c) 2010, Paulo Romulo Alves Barros <paulo.romulo@kdemail.net>
+
+find_path( HUPNP_INCLUDE_DIR HUpnp )
+
+find_library( HUPNP_LIBS HUpnp )
+
+include( FindPackageHandleStandardArgs )
+
+find_package_handle_standard_args( HUpnp "Could not find HUPnP" HUPNP_INCLUDE_DIR HUPNP_LIBS )
+
+mark_as_advanced( HUPNP_INCLUDE_DIR HUPNP_LIBS )
