725dfbcaa556afd4e597fb06af57ef0b073fa5fd
Luigi Toscano <luigi.toscano@tiscali.it>
2010-11-14 16:19:54 +0000

    Remove the dependency from the embedded DocBook XSLT, use our translations
    This commit introduces the last changes needed to get rid of the old embedded
    copy of DocBook XSLT:
    - make the common includes (kde-common-*xsl.cmake) dependent on the
      system-provided DocBook XSLTs (i.e. it generates them on the fly during
      the compilation).
    - change the macro used to generate manpages to use kde-include-man.xsl
      instead of the embedded copy of manpages generator (kde-man.xsl has never
      been used in KDE Platform 4.x as far as I know).
      It is worth noting that, on the contrary, kde-include-common.xsl is not
      used directly (because it is really common to multiple files, here
      kde-include-man.xsl really replaces the kde-man.xsl).
    - introduce a small helper program as a dependency for meinproc which will
      generate, during compile time, two xml files needed in order to make
      the system working:
      * all-l10n.xml contains the references to all the system-provided language
        files for XSLT, plus the ones not included upstream but translated in our
        repository; it is used in place of the system-provided common/l10n.xml
      * kde-custom-l10n.xml contains the references to the language files
        containing customizations for system-provided languages (included into 
        the official docbook-xsl).
    - do not install anymore the files under docbook/xsl (which will be removed)
    
    See also: http://kde.markmail.org/thread/as4ctj7kt3raws4q
    
    And now let's hope that the whole thing won't explode...
    
    CCMAIL: kde-i18n-doc@kde.org
    
    svn path=/trunk/KDE/kdelibs/; revision=1196999

--- modules/KDE4Macros.cmake
+++ modules/KDE4Macros.cmake
@@ -360,10 +360,10 @@ macro (KDE4_CREATE_MANPAGE _docbook _section)
 
    #Bootstrap
    if (_kdeBootStrapping)
-      set(_ssheet "${CMAKE_SOURCE_DIR}/kdoctools/docbook/xsl/manpages/docbook.xsl")
+      set(_ssheet "${CMAKE_BINARY_DIR}/kdoctools/customization/kde-include-man.xsl")
       set(_bootstrapOption "--srcdir=${CMAKE_BINARY_DIR}/kdoctools/")
    else (_kdeBootStrapping)
-      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/docbook/xsl/manpages/docbook.xsl")
+      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/customization/kde-include-man.xsl")
       set(_bootstrapOption)
    endif (_kdeBootStrapping)
 
