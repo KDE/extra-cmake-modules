3afcd2ce729b9dafbc18869bf1cf54ada6e0d37a
Dario Freddi <drf@kde.org>
2010-12-13 23:25:43 +0000

    Use the new polkit-qt-1 checks, and depend on polkit-qt-1 0.99.0 for that.
    svn path=/trunk/KDE/kdelibs/; revision=1206241

--- modules/FindPolkitQt-1.cmake
+++ modules/FindPolkitQt-1.cmake
@@ -18,79 +18,20 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 # Support POLKITQT-1_MIN_VERSION for compatibility:
-if(NOT PolkitQt-1_FIND_VERSION)
-  set(PolkitQt-1_FIND_VERSION "${POLKITQT-1_MIN_VERSION}")
-endif(NOT PolkitQt-1_FIND_VERSION)
+if ( NOT PolkitQt-1_FIND_VERSION AND POLKITQT-1_MIN_VERSION )
+  set ( PolkitQt-1_FIND_VERSION ${POLKITQT-1_MIN_VERSION} )
+endif ( NOT PolkitQt-1_FIND_VERSION AND POLKITQT-1_MIN_VERSION )
 
-# the minimum version of PolkitQt-1 we require
-if(NOT PolkitQt-1_FIND_VERSION)
-  set(PolkitQt-1_FIND_VERSION "0.95.1")
-endif(NOT PolkitQt-1_FIND_VERSION)
+set( _PolkitQt-1_FIND_QUIETLY  ${PolkitQt-1_FIND_QUIETLY} )
+find_package( PolkitQt-1 ${PolkitQt-1_FIND_VERSION} QUIET NO_MODULE PATHS ${LIB_INSTALL_DIR}/PolkitQt-1/cmake )
+set( PolkitQt-1_FIND_QUIETLY ${_PolkitQt-1_FIND_QUIETLY} )
 
-if (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
-   find_package(PkgConfig)
-   pkg_check_modules(PC_POLKITQT-1 QUIET polkit-qt-1)
-   set(POLKITQT-1_DEFINITIONS ${PC_POLKITQT-1_CFLAGS_OTHER})
-endif (NOT WIN32)
-
-find_path( POLKITQT-1_INCLUDE_DIR
-     NAMES polkitqt1-authority.h
-     PATH_SUFFIXES polkit-qt-1
-)
-
-find_file( POLKITQT-1_VERSION_FILE
-     NAMES polkit-qt-1/polkitqt1-version.h
-     PATHS ${POLKITQT-1_INCLUDE_DIR}
-)
-
-# Search the version and store it in the cache so we don't have to do this everytime
-if(POLKITQT-1_VERSION_FILE  AND NOT  POLKITQT-1_VERSION)
-  file(READ ${POLKITQT-1_VERSION_FILE} POLKITQT-1_VERSION_CONTENT)
-  string (REGEX MATCH "POLKITQT1_VERSION_STRING \".*\"\n" POLKITQT-1_VERSION_MATCH "${POLKITQT-1_VERSION_CONTENT}")
-
-  if(POLKITQT-1_VERSION_MATCH)
-    string(REGEX REPLACE "POLKITQT1_VERSION_STRING \"(.*)\"\n" "\\1" _POLKITQT-1_VERSION ${POLKITQT-1_VERSION_MATCH})
-  endif(POLKITQT-1_VERSION_MATCH)
-  set(POLKITQT-1_VERSION "${_POLKITQT-1_VERSION}" CACHE STRING "Version number of PolkitQt-1" FORCE)
-endif(POLKITQT-1_VERSION_FILE  AND NOT  POLKITQT-1_VERSION)
-
-find_library( POLKITQT-1_CORE_LIBRARY
-    NAMES polkit-qt-core-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-find_library( POLKITQT-1_GUI_LIBRARY
-    NAMES polkit-qt-gui-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-find_library( POLKITQT-1_AGENT_LIBRARY
-    NAMES polkit-qt-agent-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-
-set(POLKITQT-1_LIBRARIES ${POLKITQT-1_GUI_LIBRARY} ${POLKITQT-1_CORE_LIBRARY} ${POLKITQT-1_AGENT_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-
-# handle the QUIETLY and REQUIRED arguments and set POLKITQT-1_FOUND to TRUE if
-# all listed variables are TRUE
-find_package_handle_standard_args(PolkitQt-1  REQUIRED_VARS  POLKITQT-1_GUI_LIBRARY POLKITQT-1_CORE_LIBRARY
-                                                             POLKITQT-1_AGENT_LIBRARY POLKITQT-1_INCLUDE_DIR
-                                              VERSION_VAR POLKITQT-1_VERSION )
-
-mark_as_advanced(POLKITQT-1_INCLUDE_DIR
-                 POLKITQT-1_CORE_LIBRARY
-                 POLKITQT-1_GUI_LIBRARY
-                 POLKITQT-1_AGENT_LIBRARY
-                 POLKITQT-1_VERSION_FILE
-                )
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( PolkitQt-1 DEFAULT_MSG PolkitQt-1_CONFIG )
 
 if (POLKITQT-1_FOUND)
-    if (NOT PC_POLKITQT-1_PREFIX STREQUAL CMAKE_INSTALL_PREFIX)
+    if (NOT POLKITQT-1_INSTALL_DIR STREQUAL CMAKE_INSTALL_PREFIX)
         message("WARNING: Installation prefix does not match PolicyKit install prefixes. You probably will need to move files installed "
                 "in POLICY_FILES_INSTALL_DIR and by dbus_add_activation_system_service to the ${PC_POLKITQT-1_PREFIX} prefix")
-    endif (NOT PC_POLKITQT-1_PREFIX STREQUAL CMAKE_INSTALL_PREFIX)
+    endif (NOT POLKITQT-1_INSTALL_DIR STREQUAL CMAKE_INSTALL_PREFIX)
 endif (POLKITQT-1_FOUND)
-
-set(POLKITQT-1_POLICY_FILES_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/polkit-1/actions)
