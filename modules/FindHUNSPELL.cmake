c06badd8d431583071cad638fea082ca0776a0a3
David Faure <faure@kde.org>
2011-06-22 22:23:40 +0200

    Apply patch by Tomáš Chvátal to fix hunspell-1.3 detection
    ...when hunspell-1.2 is installed too.
    
    REVIEW: 101484

--- modules/FindHUNSPELL.cmake
+++ modules/FindHUNSPELL.cmake
@@ -14,7 +14,7 @@ ENDIF (HUNSPELL_INCLUDE_DIR AND HUNSPELL_LIBRARIES)
 
 FIND_PATH(HUNSPELL_INCLUDE_DIR hunspell/hunspell.hxx )
 
-FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.2 hunspell-1.3)
+FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.3 hunspell-1.2)
 
 # handle the QUIETLY and REQUIRED arguments and set HUNSPELL_FOUND to TRUE if 
 # all listed variables are TRUE
