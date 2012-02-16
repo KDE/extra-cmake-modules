83bd4b1ed69240313a887108f6f738f430896ded
Alex Neundorf <neundorf@kde.org>
2012-02-16 13:42:10 +0100

    -mark ecm_version() as obsolete, ecm_set_version_variables() should be used instead
    Alex

--- modules/ECMVersion.cmake
+++ modules/ECMVersion.cmake
@@ -1,5 +1,6 @@
 
 macro(ecm_version _major _minor _patch)
+  message(STATUS "ecm_version() is obsolete, use ecm_set_version_variables() instead")
   set(ECM_VERSION_MAJOR ${_major})
   set(ECM_VERSION_MINOR ${_minor})
   set(ECM_VERSION_PATCH ${_patch})
