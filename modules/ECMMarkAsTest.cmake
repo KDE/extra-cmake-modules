79e46741185e4d8268409b21acc2fe5a78d0d694
Alex Neundorf <neundorf@kde.org>
2012-02-16 13:36:42 +0100

    -nicer indentation
    Alex

--- modules/ECMMarkAsTest.cmake
+++ modules/ECMMarkAsTest.cmake
@@ -12,20 +12,20 @@
 # all tests by invoking the 'buildtests' target.
 
 if (NOT BUILD_TESTING)
-    get_property(_buildtestsAdded GLOBAL PROPERTY BUILDTESTS_ADDED)
-    if(NOT _buildtestsAdded)
-        add_custom_target(buildtests)
-        set_property(GLOBAL PROPERTY BUILDTESTS_ADDED TRUE)
-    endif()
+  get_property(_buildtestsAdded GLOBAL PROPERTY BUILDTESTS_ADDED)
+  if(NOT _buildtestsAdded)
+    add_custom_target(buildtests)
+    set_property(GLOBAL PROPERTY BUILDTESTS_ADDED TRUE)
+  endif()
 endif()
 
 function(ecm_mark_as_test)
   if (NOT BUILD_TESTING)
     foreach(_target ${ARGN})
       set_target_properties(${_target}
-        PROPERTIES
-        EXCLUDE_FROM_ALL 1
-      )
+                              PROPERTIES
+                              EXCLUDE_FROM_ALL TRUE
+                           )
       add_dependencies(buildtests ${_target})
     endforeach()
   endif()
