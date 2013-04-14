4d9152861f6da73a6c1e3cd4601a72fb0e3b1417
Stephen Kelly <steveire@gmail.com>
2013-04-14 09:37:41 +0200

    Remove some no-longer-needed code from Qt5Transitional.

--- find-modules/FindQt5Transitional.cmake
+++ find-modules/FindQt5Transitional.cmake
@@ -58,41 +58,6 @@ if (Qt5Core_FOUND)
   set(Qt5Transitional_FOUND TRUE)
   set(QT5_BUILD TRUE)
 
-  # Temporary until upstream does this:
-  foreach(_component ${_allComponents})
-    if (TARGET Qt5::${_component})
-      set_property(TARGET Qt5::${_component}
-        APPEND PROPERTY
-          INTERFACE_INCLUDE_DIRECTORIES ${Qt5${_component}_INCLUDE_DIRS})
-      set_property(TARGET Qt5::${_component}
-        APPEND PROPERTY
-          INTERFACE_COMPILE_DEFINITIONS ${Qt5${_component}_COMPILE_DEFINITIONS})
-    endif()
-  endforeach()
-
-  set_property(TARGET Qt5::Core
-        PROPERTY
-          INTERFACE_POSITION_INDEPENDENT_CODE ON
-  )
-
-  if (WIN32 AND NOT Qt5_NO_LINK_QTMAIN)
-      set(_isExe $<STREQUAL:$<TARGET_PROPERTY:TYPE>,EXECUTABLE>)
-      set(_isWin32 $<BOOL:$<TARGET_PROPERTY:WIN32_EXECUTABLE>>)
-      set(_isNotExcluded $<NOT:$<BOOL:$<TARGET_PROPERTY:Qt5_NO_LINK_QTMAIN>>>)
-      get_target_property(_configs Qt5::Core IMPORTED_CONFIGURATIONS)
-      foreach(_config ${_configs})
-          set_property(TARGET Qt5::Core APPEND PROPERTY
-              IMPORTED_LINK_INTERFACE_LIBRARIES_${_config}
-                  $<$<AND:${_isExe},${_isWin32},${_isNotExcluded}>:Qt5::WinMain>
-          )
-      endforeach()
-      unset(_configs)
-      unset(_isExe)
-      unset(_isWin32)
-      unset(_isNotExcluded)
-  endif()
-  # End upstreamed stuff.
-
   get_filename_component(_modules_dir "${CMAKE_CURRENT_LIST_DIR}/../modules" ABSOLUTE)
   include("${_modules_dir}/ECMQt4To5Porting.cmake") # TODO: Port away from this.
 
@@ -106,7 +71,6 @@ else()
     list(APPEND _components Qt${_component})
   endforeach()
   find_package(Qt4 ${QT_MIN_VERSION} REQUIRED ${_components})
-  include_directories(${QT_INCLUDES})
 
   if(QT4_FOUND)
     set(Qt5Transitional_FOUND TRUE)
