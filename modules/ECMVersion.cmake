75887108189813f3c05266e863041cd47a986100
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2013-09-11 12:50:01 +0200

    Drop ecm_version and ecm_write_version_header
    Both macros are obsoleted by ecm_setup_version.
    
    REVIEW: 112665

--- modules/ECMVersion.cmake
+++
@@ -1,9 +0,0 @@
-
-macro(ecm_version _major _minor _patch)
-  message(STATUS "************** ecm_version() is deprecated")
-  set(ECM_VERSION_MAJOR ${_major})
-  set(ECM_VERSION_MINOR ${_minor})
-  set(ECM_VERSION_PATCH ${_patch})
-  set(ECM_SOVERSION ${_major})
-  set(ECM_VERSION_STRING "${ECM_VERSION_MAJOR}.${ECM_VERSION_MINOR}.${ECM_VERSION_PATCH}")
-endmacro()
