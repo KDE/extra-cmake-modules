782010ea1e2a441a566e4a7b78c711cfece896ed
Albert Astals Cid <aacid@kde.org>
2013-12-14 16:25:57 +0100

    Split Frameworks only definitions to a new file

---
+++ kde-modules/KDEFrameworkCompilerSettings.cmake
@@ -0,0 +1,11 @@
+include(KDECompilerSettings)
+
+add_definitions(-DQT_NO_CAST_TO_ASCII
+                -DQT_NO_CAST_FROM_ASCII
+                -DQT_STRICT_ITERATORS
+                -DQT_NO_URL_CAST_FROM_STRING
+                -DQT_NO_CAST_FROM_BYTEARRAY
+                -DQT_NO_SIGNALS_SLOTS_KEYWORDS
+                -DQT_USE_FAST_CONCATENATION
+                -DQT_USE_FAST_OPERATOR_PLUS
+               )
