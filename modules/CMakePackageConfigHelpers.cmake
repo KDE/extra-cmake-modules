a68906cfa8ed4cba4aee06e8f17c7164cfe216df
Alex Merry <kde@randomguy3.me.uk>
2014-02-18 22:36:13 +0000

    Remove CMakePackageConfigHelpers.cmake
    All the frameworks are ported now, so this is no longer necessary.
    Checked by building everything with kdesrc-build.

--- modules/CMakePackageConfigHelpers.cmake
+++
@@ -1,9 +0,0 @@
-# Compatibility hack to make porting frameworks easier
-# TODO: remove before releasing 1.0.0
-
-include(${CMAKE_CURRENT_LIST_DIR}/ECMPackageConfigHelpers.cmake)
-
-function(CONFIGURE_PACKAGE_CONFIG_FILE)
-    message(AUTHOR_WARNING "Use ecm_configure_package_config_file() from ECMPackageConfigHelpers instead of configure_package_config_file()")
-    ecm_configure_package_config_file(${ARGV})
-endfunction()
