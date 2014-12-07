fd7aa7e8eab35c81faaeb8346a57b73dd63f4efb
Alex Merry <alex.merry@kde.org>
2014-12-07 19:17:34 +0000

    Fix ECMQueryQmake when Qt5Core is found.
    81627ad86d3d7d5e5a7d130dfc746d5b1b58cbe7 broke the case where Qt5 qmake
    is not in the path or not called qmake-qt5, because it stopped using the
    location of qmake as provided by the Qt5Core CMake module when found.

--- modules/ECMQueryQmake.cmake
+++ modules/ECMQueryQmake.cmake
@@ -4,7 +4,7 @@ set(_qmake_executable_default "qmake-qt5")
 if (TARGET Qt5::qmake)
     get_target_property(_qmake_executable_default Qt5::qmake LOCATION)
 endif()
-set(QMAKE_EXECUTABLE "qmake-qt5"
+set(QMAKE_EXECUTABLE ${_qmake_executable_default}
     CACHE FILEPATH "Location of the Qt5 qmake executable")
 
 # This is not public API (yet)!
@@ -14,8 +14,8 @@ function(query_qmake result_variable qt_variable)
         RESULT_VARIABLE return_code
         OUTPUT_VARIABLE output
     )
-    string(STRIP ${output} output)
     if(return_code EQUAL 0)
+        string(STRIP "${output}" output)
         file(TO_CMAKE_PATH "${output}" output_path)
         set(${result_variable} "${output_path}" PARENT_SCOPE)
     else()
