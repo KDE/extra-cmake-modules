KDE/extra-cmake-modules

commit fb7b8eea7d91772f989d5b060c86df20f2ebdb66

Fix ECMInstallIconsTest.
ECMInstallIcons now updates the theme cache if gtk-update-icon-cache is
available, producing files the test hadn't been expecting.. Updating the
test revealed that the old-style ecm_install_icons call only updated the
hicolor cache, and not any of the other themes.

REVIEW: 125631

modules/ECMInstallIcons.cmake
@@ -111,6 +111,8 @@ macro(_ecm_install_icons_v1 _defaultpath)
       set(_l10n_SUBDIR ".")
    endif(_lang)
 
+   set(_themes)
+
    # first the png icons
    file(GLOB _icons *.png)
    foreach (_current_ICON ${_icons} )
@@ -123,6 +125,7 @@ macro(_ecm_install_icons_v1 _defaultpath)
 
       set(_theme_GROUP ${_ECM_ICON_THEME_${_type}})
       if( _theme_GROUP)
+         list(APPEND _themes "${_theme_GROUP}")
          _ECM_ADD_ICON_INSTALL_RULE(${CMAKE_CURRENT_BINARY_DIR}/install_icons.cmake
                     ${_defaultpath}/${_theme_GROUP}/${_size}x${_size}
                     ${_group} ${_current_ICON} ${_name} ${_l10n_SUBDIR})
@@ -141,6 +144,7 @@ macro(_ecm_install_icons_v1 _defaultpath)
 
       set(_theme_GROUP ${_ECM_ICON_THEME_${_type}})
       if( _theme_GROUP)
+         list(APPEND _themes "${_theme_GROUP}")
          _ECM_ADD_ICON_INSTALL_RULE(${CMAKE_CURRENT_BINARY_DIR}/install_icons.cmake
                 ${_defaultpath}/${_theme_GROUP}/${_size}x${_size}
                 ${_group} ${_current_ICON} ${_name} ${_l10n_SUBDIR})
@@ -158,13 +162,17 @@ macro(_ecm_install_icons_v1 _defaultpath)
 
       set(_theme_GROUP ${_ECM_ICON_THEME_${_type}})
       if( _theme_GROUP)
+         list(APPEND _themes "${_theme_GROUP}")
           _ECM_ADD_ICON_INSTALL_RULE(${CMAKE_CURRENT_BINARY_DIR}/install_icons.cmake
                             ${_defaultpath}/${_theme_GROUP}/scalable
                             ${_group} ${_current_ICON} ${_name} ${_l10n_SUBDIR})
       endif( _theme_GROUP)
    endforeach (_current_ICON)
 
-   _ecm_update_iconcache("${_defaultpath}" hicolor)
+   list(REMOVE_DUPLICATES _themes)
+   foreach(_theme ${_themes})
+       _ecm_update_iconcache("${_defaultpath}" "${_theme}")
+   endforeach()
 
 endmacro()
 

tests/ECMInstallIconsTest/CMakeLists.txt
@@ -94,5 +94,12 @@ ecm_install_icons(
     DESTINATION badly-named-files-test
 )
 
+find_program(icon_cache_generator NAMES gtk-update-icon-cache)
+if (icon_cache_generator)
+    set(GENERATE_ICON_CACHE TRUE)
+else()
+    set(GENERATE_ICON_CACHE FALSE)
+endif()
+
 # this will be run by CTest
 configure_file(check_tree.cmake.in "${CMAKE_CURRENT_BINARY_DIR}/check_tree.cmake" @ONLY)

tests/ECMInstallIconsTest/check_tree.cmake.in
@@ -1,8 +1,15 @@
 set(EXP_TREE "@CMAKE_CURRENT_SOURCE_DIR@/expected-tree")
 set(ACTUAL_TREE "@CMAKE_INSTALL_PREFIX@")
+set(GENERATE_ICON_CACHE "@GENERATE_ICON_CACHE@")
 
 file(GLOB_RECURSE exp_files RELATIVE "${EXP_TREE}" "${EXP_TREE}/*")
 file(GLOB_RECURSE actual_files RELATIVE "${ACTUAL_TREE}" "${ACTUAL_TREE}/*")
+if (NOT GENERATE_ICON_CACHE)
+    file(GLOB_RECURSE cache_files RELATIVE "${EXP_TREE}" "${EXP_TREE}/*.cache")
+    foreach(f ${cache_files})
+        list(REMOVE_ITEM exp_files "${f}")
+    endforeach()
+endif()
 list(SORT exp_files)
 list(SORT actual_files)
 

commit 21629f651a6a5d9d977be03fd9f98417c4fa27ae

Warn instead of error if ecm_install_icons finds no icons.
The V1 syntax of ecm_install_icons searched for icons by globbing files
with a particular naming pattern. If there were no such icons, this used
to do nothing, but silently. Commit fb7b8eea7d accidentally made this an
error. More sensible would be to make it a warning.

BUG: 354610
REVIEW: 125931

modules/ECMInstallIcons.cmake
@@ -169,10 +169,14 @@ macro(_ecm_install_icons_v1 _defaultpath)
       endif( _theme_GROUP)
    endforeach (_current_ICON)
 
-   list(REMOVE_DUPLICATES _themes)
-   foreach(_theme ${_themes})
-       _ecm_update_iconcache("${_defaultpath}" "${_theme}")
-   endforeach()
+   if (_themes)
+       list(REMOVE_DUPLICATES _themes)
+       foreach(_theme ${_themes})
+           _ecm_update_iconcache("${_defaultpath}" "${_theme}")
+       endforeach()
+   else()
+       message(AUTHOR_WARNING "No suitably-named icons found")
+   endif()
 
 endmacro()
 

tests/ECMInstallIconsTest/CMakeLists.txt
@@ -10,6 +10,7 @@ include(ECMInstallIcons)
 
 add_subdirectory(v1-syntax)
 add_subdirectory(v1-syntax-l10n)
+add_subdirectory(v1-syntax-no-icons)
 
 ecm_install_icons(
     ICONS 16-actions-computer.png

tests/ECMInstallIconsTest/v1-syntax-no-icons/CMakeLists.txt
@@ -1,0 +1,1 @@
+ecm_install_icons(v1-icons-dummy)