c6ae4fa9008b0805f4358012040e67577ba31791
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2016-06-13 18:27:24 +0200

    Integrate relative library paths to APK
    Needed for many unit tests to add them to APK files.
    
    REVIEW: 128175

--- toolchain/specifydependencies.cmake
+++ toolchain/specifydependencies.cmake
@@ -2,13 +2,17 @@ file(READ "${TARGET_DIR}/CMakeFiles/${TARGET_NAME}.dir/link.txt" out)
 
 string(FIND "${out}" "-o ${TARGET_NAME}" POS) #we trim the initial arguments, we want the ones in the end. we find the target
 string(SUBSTRING "${out}" ${POS} -1 out) #we
-string(REGEX MATCHALL " /.+\\.so" outout "${out}")
+string(REGEX MATCHALL "(/|\\.\\./|\\./)[^ ]+\\.so" outout "${out}")
 string(STRIP "${outout}" outout)
 string(REPLACE " /" ";/" outout "${outout}")
 
 set(extralibs)
 foreach(lib IN LISTS outout) #now we filter Qt5 libraries, because Qt wants to take care about these itself
     if(NOT ${lib} MATCHES ".*/libQt5.*")
+        # resolve relative paths
+        if(${lib} MATCHES "^(\\.\\./|\\./)")
+            set(lib "${TARGET_DIR}/${lib}")
+        endif()
         if(extralibs)
             set(extralibs "${extralibs},${lib}")
         else()
