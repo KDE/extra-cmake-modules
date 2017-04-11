78529f4452b2e09d9f579d7723f7cb7634f43e7c
Aleix Pol <aleixpol@kde.org>
2017-04-11 15:49:38 +0200

    Allow test to work with older versions of qmlplugindump

--- modules/test_execute_and_compare.cmake
+++ modules/test_execute_and_compare.cmake
@@ -1,6 +1,6 @@
 string(REPLACE / ";" processedArgs "${ARG_UNPARSED_ARGUMENTS}")
 
-execute_process(COMMAND qmlplugindump -nonrelocatable ${processedArgs} ${KDE_INSTALL_QMLDIR} -output ${CMAKE_CURRENT_SOURCE_DIR}/plugins.qmltypes RESULT_VARIABLE code)
+execute_process(COMMAND qmlplugindump -nonrelocatable ${processedArgs} ${KDE_INSTALL_QMLDIR} > ${CMAKE_CURRENT_SOURCE_DIR}/plugins.qmltypes RESULT_VARIABLE code)
 if(code)
     message(FATAL_ERROR "Could not call qmlplugindump successfully. Exited with code ${code}")
 endif()
