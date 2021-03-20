db1f649cf9f324efef8a00c9a74c667a95118174
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-03-20 09:06:33 +0100

    KDEGitCommitHooks: Make clang-format hook more interactive
    In case the user commits in a terminal we output the diff and
    present options to format the files and add all tracked files or just
    apply the formatting and manually stage the files afterwards.
    
    In case the user uses not a terminal we output the diff to make it
    easier to review the changes. Then the clang-format integration of the
    IDE/text editor or the suggested `git clang-format -f` command can be
    used to apply the formatting.

--- kde-modules/kde-git-commit-hooks/clang-format.sh
+++ kde-modules/kde-git-commit-hooks/clang-format.sh
@@ -2,12 +2,36 @@
 
 # Based on okular/hooks/pre-commit, credits go to Albert Astals Cid
 
-readonly output=$(git clang-format -v --diff)
+readonly output=$(git clang-format --diff)
 
 if [[ "$output" == *"no modified files to format"* ]]; then exit 0; fi
 if [[ "$output" == *"clang-format did not modify any files"* ]]; then exit 0; fi
 
-echo "ERROR: You have unformatted changes, please format your files. You can do this using the following commands:"
-echo "       git clang-format --force # format the changed parts"
-echo "       git clang-format --diff # preview the changes done by the formatter"
+display_usage_help () {
+    echo "ERROR: You have unformatted changes, please format your files. You can do this using the following commands:"
+    echo "       git clang-format --force # format the changed parts"
+    echo "       git clang-format --diff # preview the changes done by the formatter"
+}
+
+# Output the diff if we are already in a shell
+if [ -t 1 ]
+then
+    echo ""
+    echo "The \"git clang-format --diff\" output is:"
+    git clang-format --diff
+    echo ""
+    echo "Do you want to a) apply the formatting and add all tracked files, y) format the files and exit and n) to abort a/y/n"
+    read -p "" response < /dev/tty
+    while true; do
+        case $response in
+            [Aa]* ) git clang-format -f; git add -u; exit 0; break;;
+            [Yy]* ) git clang-format -f; exit 1; break;;
+            [Nn]* ) display_usage_help; exit 1;;
+            * ) echo "Please answer a or y or n";;
+        esac
+    done
+else
+    echo "$output"
+    display_usage_help
+fi
 exit 1
