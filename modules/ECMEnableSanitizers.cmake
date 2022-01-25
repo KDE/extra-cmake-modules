967548a961220b3bd6ad25fdf600ca59f5845434
Ahmad Samir <a.samirh78@gmail.com>
2022-01-25 23:56:41 +0200

    Fix building ECM with CMake < 3.18
    CheckLinkerFlags is available since CMake 3.18, for older versions assume
    ASAN linker flags are supported to restore the old behaviour and keep ECM
    buildable with older CMake versions.

--- modules/ECMEnableSanitizers.cmake
+++ modules/ECMEnableSanitizers.cmake
@@ -74,7 +74,12 @@ This is an example of usage::
 Since 1.3.0.
 #]=======================================================================]
 
-include(CheckLinkerFlag)
+set(_HAVE_CHECK_LINKER FALSE)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
+    include(CheckLinkerFlag)
+    set(_HAVE_CHECK_LINKER TRUE)
+endif()
+
 # MACRO check_compiler_version
 #-----------------------------
 macro (check_compiler_version gcc_required_version clang_required_version msvc_required_version)
@@ -148,8 +153,11 @@ if (ECM_ENABLE_SANITIZERS)
             string(TOLOWER ${CUR_SANITIZER} CUR_SANITIZER)
             # check option and enable appropriate flags
             enable_sanitizer_flags ( ${CUR_SANITIZER} )
-            check_linker_flag(CXX "-l${XSAN_LINKER_FLAGS}" _HAVE_XSAN_LINKER_FLAGS)
-            if (_HAVE_XSAN_LINKER_FLAGS)
+            if(_HAVE_CHECK_LINKER)
+                check_linker_flag(CXX "-l${XSAN_LINKER_FLAGS}" _HAVE_XSAN_LINKER_FLAGS)
+            endif()
+            # check_linker_flag is available only since 3.18, for older CMake versions keep the old behaviour
+            if (_HAVE_XSAN_LINKER_FLAGS OR NOT _HAVE_CHECK_LINKER)
               # TODO: GCC will not link pthread library if enabled ASan
               if(CMAKE_C_COMPILER_ID MATCHES "Clang")
                 string(APPEND CMAKE_C_FLAGS " ${XSAN_COMPILE_FLAGS}")
