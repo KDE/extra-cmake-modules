178c4dfbf4eb5ad7c5aef4838f190f717f97766f
Ahmad Samir <a.samirh78@gmail.com>
2022-08-08 23:28:13 +0200

    clang-format.cmake: don't change spaces after '//' in code comments
    For example, if you have:
    //This is a comment
    
    now clang-format won't change it to:
    // This is a comment
    
    We're mainly interested in formatting the code, not the comments, so that
    cuts down on some noise when running clang-format.
    
    CCBUG: 456819

--- kde-modules/clang-format.cmake
+++ kde-modules/clang-format.cmake
@@ -86,3 +86,8 @@ AllowShortLambdasOnASingleLine: Empty
 
 # We do not want clang-format to put all arguments on a new line
 AllowAllArgumentsOnNextLine: false
+
+# Don't change spaces after '//' in code comments
+SpacesInLineCommentPrefix:
+    Minimum: 0
+    Maximum: -1
