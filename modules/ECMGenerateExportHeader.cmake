KDE/extra-cmake-modules

commit c6e575539a4c0f370df65d15001e7f1b942b7e91

Clean ECM files after the minimum version change
- Remove deprecated version checks
- Use VERSION_GREATER_EQUAL

modules/ECMGenerateExportHeader.cmake
@@ -394,9 +394,7 @@ function(_ecm_geh_generate_hex_number _var_name _version)
     string(REGEX REPLACE ${version_regex} "\\2" _version_minor "${_version}")
     string(REGEX REPLACE ${version_regex} "\\3" _version_patch "${_version}")
     set(_outputformat)
-    if (NOT CMAKE_VERSION VERSION_LESS 3.13)
-        set(_outputformat OUTPUT_FORMAT HEXADECIMAL)
-    endif()
+    set(_outputformat OUTPUT_FORMAT HEXADECIMAL)
     math(EXPR _hexnumber "${_version_major}*65536 + ${_version_minor}*256 + ${_version_patch}" ${_outputformat})
     set(${_var_name} ${_hexnumber} PARENT_SCOPE)
 endfunction()
@@ -470,9 +468,6 @@ function(ecm_generate_export_header target)
         message(FATAL_ERROR "No VERSION passed when calling ecm_generate_export_header().")
     elseif(NOT ARGS_VERSION MATCHES ${_version_triple_regexp})
         message(FATAL_ERROR "VERSION expected to be in x.y.z format when calling ecm_generate_export_header().")
-    endif()
-    if (ARGS_INCLUDE_GUARD_NAME AND CMAKE_VERSION VERSION_LESS 3.11)
-        message(FATAL_ERROR "Argument INCLUDE_GUARD_NAME needs at least CMake 3.11 when calling ecm_generate_export_header().")
     endif()
     if (NOT ARGS_EXCLUDE_DEPRECATED_BEFORE_AND_AT)
         set(ARGS_EXCLUDE_DEPRECATED_BEFORE_AND_AT 0)
@@ -771,9 +766,7 @@ function(ecm_generate_export_header target)
     # for older cmake verions we have to manually append our generated content
     # for newer we use CUSTOM_CONTENT_FROM_VARIABLE
     set(_custom_content_args)
-    if (NOT CMAKE_VERSION VERSION_LESS 3.7)
-        set(_custom_content_args CUSTOM_CONTENT_FROM_VARIABLE _output)
-    endif()
+    set(_custom_content_args CUSTOM_CONTENT_FROM_VARIABLE _output)
     generate_export_header(${target}
         BASE_NAME ${ARGS_BASE_NAME}
         DEPRECATED_MACRO_NAME "${_macro_base_name}_DECL_DEPRECATED"
@@ -786,14 +779,13 @@ function(ecm_generate_export_header target)
         ${_custom_content_args}
     )
 
-    if (CMAKE_VERSION VERSION_LESS 3.7)
-        if (ARGS_INCLUDE_GUARD_NAME)
-            set(_include_guard "ECM_GENERATEEXPORTHEADER_${ARGS_INCLUDE_GUARD_NAME}")
-        else()
-            set(_include_guard "ECM_GENERATEEXPORTHEADER_${_upper_base_name}_EXPORT_H")
-        endif()
+    if (ARGS_INCLUDE_GUARD_NAME)
+        set(_include_guard "ECM_GENERATEEXPORTHEADER_${ARGS_INCLUDE_GUARD_NAME}")
+    else()
+        set(_include_guard "ECM_GENERATEEXPORTHEADER_${_upper_base_name}_EXPORT_H")
+    endif()
 
-        file(APPEND ${_header_work_file} "
+    file(APPEND ${_header_work_file} "
 
 #ifndef ${_include_guard}
 #define ${_include_guard}
@@ -802,8 +794,7 @@ ${_output}
 
 #endif /* ${_include_guard} */
 "
-        )
-    endif()
+    )
 
     # avoid rebuilding if there was no change
     execute_process(

find-modules/FindKF5.cmake
@@ -55,7 +55,7 @@ foreach(_module ${KF5_FIND_COMPONENTS})
     )
     # CMake >= 3.17 wants to be explicitly told we are fine with name mismatch here
     set(_name_mismatched_arg)
-    if(NOT CMAKE_VERSION VERSION_LESS 3.17)
+    if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.17)
        set(_name_mismatched_arg NAME_MISMATCHED)
     endif()
     find_package_handle_standard_args(KF5${_module} CONFIG_MODE ${_name_mismatched_arg})

kde-modules/KDECMakeSettings.cmake
@@ -220,9 +220,7 @@ if(NOT KDE_SKIP_BUILD_SETTINGS)
    # Since CMake 3.0
    # TODO KF6: discuss enabling AUTOUIC and note porting requirements. autouic
    #   acts on all #include "ui_*.h" assuming *.ui exists
-   if(NOT CMAKE_VERSION VERSION_LESS 3.0)
-      set(CMAKE_AUTORCC ON)
-   endif()
+   set(CMAKE_AUTORCC ON)
 
    # By default, create 'GUI' executables. This can be reverted on a per-target basis
    # using ECMMarkNonGuiExecutable
@@ -246,7 +244,7 @@ if(NOT KDE_SKIP_BUILD_SETTINGS)
    # even without installation.
 
    # We do the same under Unix to make it possible to run tests and apps without installing
-   if (WIN32 OR NOT ("${ECM_GLOBAL_FIND_VERSION}" VERSION_LESS "5.38.0"))
+   if (WIN32 OR ECM_GLOBAL_FIND_VERSION VERSION_GREATER_EQUAL 5.38.0)
        set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
        set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
@@ -342,9 +340,7 @@ if(NOT EXISTS ${CMAKE_SOURCE_DIR}/po AND NOT TARGET fetch-translations)
     set(_l10n_po_dir "${CMAKE_BINARY_DIR}/po")
     set(_l10n_poqm_dir "${CMAKE_BINARY_DIR}/poqm")
 
-    if(CMAKE_VERSION VERSION_GREATER 3.2)
-        set(extra BYPRODUCTS ${_l10n_po_dir} ${_l10n_poqm_dir})
-    endif()
+    set(extra BYPRODUCTS ${_l10n_po_dir} ${_l10n_poqm_dir})
 
     set(fetch_commands
         COMMAND ruby "${CMAKE_BINARY_DIR}/releaseme/fetchpo.rb"

kde-modules/KDECompilerSettings.cmake
@@ -75,7 +75,7 @@ endif()
 ############################################################
 
 macro(_kde_compiler_min_version min_version)
-    if ("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS "${min_version}")
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${min_version})
         message(WARNING "Version ${CMAKE_CXX_COMPILER_VERSION} of the ${CMAKE_CXX_COMPILER_ID} C++ compiler is not supported. Please use version ${min_version} or later.")
     endif()
 endmacro()
@@ -368,12 +368,12 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror=return-type")
 endif()
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR
-    (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5))
+    (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.5))
     # -Wvla: use of variable-length arrays (an extension to C++)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wvla")
 endif()
-if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0) OR
-    (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5))
+if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 5.0) OR
+    (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.5))
     include(CheckCXXCompilerFlag)
     check_cxx_compiler_flag(-Wdate-time HAVE_DATE_TIME)
     if (HAVE_DATE_TIME)
@@ -383,7 +383,7 @@ if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VER
 endif()
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-   if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0")
+   if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 5.0.0)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wsuggest-override -Wlogical-op" )
    endif()
 endif()
@@ -461,8 +461,8 @@ if (MINGW AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 endif()
 
 if (CMAKE_GENERATOR STREQUAL "Ninja" AND
-    ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9) OR
-     (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)))
+    ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 4.9) OR
+     (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.5)))
     # Force colored warnings in Ninja's output, if the compiler has -fdiagnostics-color support.
     # Rationale in https://github.com/ninja-build/ninja/issues/814
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")

kde-modules/KDEFrameworkCompilerSettings.cmake
@@ -45,7 +45,7 @@ endif()
 
 # Some non-KF projects make (ab)use of KDEFrameworkCompilerSettings currently,
 # let them only hit this as well when bumping their min. ECM requirement to a newer version.
-if (NOT "${ECM_GLOBAL_FIND_VERSION}" VERSION_LESS "5.79.0")
+if (ECM_GLOBAL_FIND_VERSION VERSION_GREATER_EQUAL 5.79.0)
     add_definitions(
         -DQT_NO_KEYWORDS
         -DQT_NO_FOREACH
@@ -64,18 +64,18 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang
 endif()
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-   if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0")
+   if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 5.0.0)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wzero-as-null-pointer-constant" )
    endif()
 endif()
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-   if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0")
+   if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 5.0.0)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wzero-as-null-pointer-constant" )
    endif()
 endif()
 
-if (NOT ("${ECM_GLOBAL_FIND_VERSION}" VERSION_LESS "5.80.0"))
+if (ECM_GLOBAL_FIND_VERSION VERSION_GREATER_EQUAL 5.80.0)
     include(KDEClangFormat)
     # add clang-format target
     file(GLOB_RECURSE ALL_CLANG_FORMAT_SOURCE_FILES *.cpp *.h *.c)

kde-modules/KDEInstallDirs.cmake
@@ -334,9 +334,7 @@ macro(_define_relative varname parent subdir docstring)
                 PROPERTY TYPE PATH)
         endif()
     elseif(${_oldstylename})
-        if(NOT CMAKE_VERSION VERSION_LESS 3.0.0)
-            message(DEPRECATION "${_oldstylename} is deprecated, use KDE_INSTALL_${varname} instead.")
-        endif()
+        message(DEPRECATION "${_oldstylename} is deprecated, use KDE_INSTALL_${varname} instead.")
         # The old name was given (probably on the command line): move
         # it to the new name
         set(KDE_INSTALL_${varname} "${${_oldstylename}}"
@@ -344,7 +342,7 @@ macro(_define_relative varname parent subdir docstring)
                   "${docstring} (${_docpath})"
                   FORCE)
     elseif(${_cmakename})
-        if(_cmakename_is_deprecated AND NOT CMAKE_VERSION VERSION_LESS 3.0.0)
+        if(_cmakename_is_deprecated)
             message(DEPRECATION "${_cmakename} is deprecated, use KDE_INSTALL_${varname} instead.")
         endif()
         # The CMAKE_ name was given (probably on the command line): move
@@ -417,7 +415,6 @@ if(APPLE)
         BUNDLE_INSTALL_DIR)
 endif()
 
-# Only supported since cmake 3.7
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     set(CMAKE_INSTALL_PREFIX "${ECM_PREFIX}" CACHE PATH "Install path prefix" FORCE)
 endif()

kde-modules/KDEPackageAppTemplates.cmake
@@ -107,19 +107,9 @@ function(kde_package_app_templates)
         get_filename_component(_baseName ${_tmp_file} NAME_WE)
         set(_template ${CMAKE_CURRENT_BINARY_DIR}/${_baseName}.tar.bz2)
 
-        # CONFIGURE_DEPENDS is only available for cmake >= 3.12
-        if(NOT CMAKE_VERSION VERSION_LESS "3.12.0")
-            # also enlist directories as deps to catch file removals
-            file(GLOB_RECURSE _subdirs_entries LIST_DIRECTORIES true CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*")
-        else()
-            file(GLOB_RECURSE _subdirs_entries LIST_DIRECTORIES true "${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*")
-            # custom code to implement CONFIGURE_DEPENDS
-            foreach(_direntry ${_subdirs_entries})
-                if(IS_DIRECTORY ${_direntry})
-                    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${_direntry})
-                endif()
-            endforeach()
-        endif()
+        # also enlist directories as deps to catch file removals
+        file(GLOB_RECURSE _subdirs_entries LIST_DIRECTORIES true CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*")
+
         add_custom_target(${_baseName} ALL DEPENDS ${_template})
 
         if(GNU_TAR_FOUND)

modules/ECMAddQtDesignerPlugin.cmake
@@ -618,7 +618,7 @@ QList<QDesignerCustomWidgetInterface*> ${_collection_classname}::customWidgets()
 
         # setup plugin binary
         add_library(${target} MODULE ${_srcs})
-        if(Qt5UiPlugin_VERSION AND NOT Qt5UiPlugin_VERSION VERSION_LESS "5.9.0")
+        if(Qt5UiPlugin_VERSION AND Qt5UiPlugin_VERSION VERSION_GREATER_EQUAL 5.9.0)
             list(APPEND ARGS_LINK_LIBRARIES Qt5::UiPlugin)
         else()
             # For Qt <5.9 include dir variables needed

commit c3741f922027d68e856cfb5622cd8986dc5a0805

ECMGenerateExportHeader: fix duplicated addition of deprecation macros code
And clean up other remaining code for older cmake versions.

c6e575539a4c0f370df65d15001e7f1b942b7e91 accidentally moved the logic
for adding the deprecation macro code for older cmake versions next to
the logic for adding it with newer cmake versions, instead of dropping it.

modules/ECMGenerateExportHeader.cmake
@@ -749,9 +749,6 @@ function(ecm_generate_export_header target)
         endif()
     endif()
 
-    set(_header_file "${ARGS_EXPORT_FILE_NAME}")
-    set(_header_work_file "${_header_file}.work")
-
     # prepare optional arguments to pass through to generate_export_header
     set(_include_guard_name_args)
     if (ARGS_INCLUDE_GUARD_NAME)
@@ -773,10 +770,6 @@ function(ecm_generate_export_header target)
     if (ARGS_STATIC_DEFINE)
         set(_static_define_args STATIC_DEFINE "${ARGS_STATIC_DEFINE}")
     endif()
-    # for older cmake versions we have to manually append our generated content
-    # for newer we use CUSTOM_CONTENT_FROM_VARIABLE
-    set(_custom_content_args)
-    set(_custom_content_args CUSTOM_CONTENT_FROM_VARIABLE _output)
     generate_export_header(${target}
         BASE_NAME ${ARGS_BASE_NAME}
         DEPRECATED_MACRO_NAME "${_macro_base_name}_DECL_DEPRECATED"
@@ -784,31 +777,8 @@ function(ecm_generate_export_header target)
         ${_export_macro_name_args}
         ${_no_export_macro_name_args}
         ${_static_define_args}
-        EXPORT_FILE_NAME "${_header_work_file}"
+        EXPORT_FILE_NAME "${ARGS_EXPORT_FILE_NAME}"
         ${_include_guard_name_args}
-        ${_custom_content_args}
+        CUSTOM_CONTENT_FROM_VARIABLE _output
     )
-
-    if (ARGS_INCLUDE_GUARD_NAME)
-        set(_include_guard "ECM_GENERATEEXPORTHEADER_${ARGS_INCLUDE_GUARD_NAME}")
-    else()
-        set(_include_guard "ECM_GENERATEEXPORTHEADER_${_upper_base_name}_EXPORT_H")
-    endif()
-
-    file(APPEND ${_header_work_file} "
-
-#ifndef ${_include_guard}
-#define ${_include_guard}
-
-${_output}
-
-#endif /* ${_include_guard} */
-"
-    )
-
-    # avoid rebuilding if there was no change
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${_header_work_file}" "${_header_file}"
-    )
-    file(REMOVE "${_header_work_file}")
 endfunction()