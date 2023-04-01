a0528c09e53e2e1443a3de8a9989cf052a1bba30
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-04-01 11:54:23 +0200

    ECMFindQmlModule: Make version optional
    In Qt6, we also do not need the version in the QML imports

--- modules/ECMFindQmlModule.cmake
+++ modules/ECMFindQmlModule.cmake
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2015 Aleix Pol Gonzalez <aleixpol@blue-systems.com>
+# SPDX-FileCopyrightText: 2023 Alexander Lohnau <alexander.lohnau@gmx.de>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -16,10 +17,9 @@ but fail at runtime.
 
 ::
 
-  ecm_find_qmlmodule(<module_name> <version>...)
-
-Any further arguments passed will be forwarded into a ``find_package()`` call. See
-``find_package()`` documentation for more information.
+  ecm_find_qmlmodule(<module_name>
+    [version] # Optional for Qt6 builds
+  )
 
 Usage example:
 
@@ -32,11 +32,21 @@ Since 5.38.0.
 
 set(MODULES_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-function(ecm_find_qmlmodule MODULE_NAME VERSION)
+function(ecm_find_qmlmodule MODULE_NAME)
+    list(LENGTH ARGN ARGC)
+    if (QT_MAJOR_VERSION STREQUAL 6)
+      if (ARGC EQUAL 1)
+        list(GET ARGN 0 VERSION)
+      endif()
+    else()
+      if (ARGC EQUAL 2)
+        list(GET ARGN 1 VERSION)
+      else()
+        message(FATAL_ERROR "No version provided for ecm_find_qmlmodule, this is required for Qt5 builds")
+      endif()
+    endif()
     set(GENMODULE "${MODULE_NAME}-QMLModule")
-
     configure_file("${MODULES_DIR}/ECMFindQmlModule.cmake.in" "Find${GENMODULE}.cmake" @ONLY)
-
     set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_BINARY_DIR}" ${CMAKE_MODULE_PATH})
     find_package(${GENMODULE} ${ARGN})
 
