a068eaba79593910d000358330187f542d5b6eb3
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-07-21 16:05:23 +0200

    KDEGitCommitHooks: Add commit hook for JSON files
    This way one will notice issues before having committed these files.
    The schema files are dynamically picked up during configuring, meaning
    projects can provide their own schema.
    This mechanism required an identifier to know when the validation should
    be triggered. For the KPluginMetaData schema that is the "KPlugin"
    object.

--- kde-modules/KDEGitCommitHooks.cmake
+++ kde-modules/KDEGitCommitHooks.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Alexander Lohnau <alexander.lohnau@gmx.de>
+# SPDX-FileCopyrightText: 2020-2023 Alexander Lohnau <alexander.lohnau@gmx.de>
 # SPDX-FileCopyrightText: 2022 Ahmad Samir <a.samirh78@gmail.com>
 # SPDX-FileCopyrightText: 2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
 #
@@ -33,26 +33,35 @@ Checks:
   want to use the one provided by ECM you can include ``include(KDEClangFormat)`` which will copy
   the file to the source dir. It is also recommended to reformat the entire project before enforcing
   the formatting using this commit hook.
+  - ``JSON_SCHEMA`` Since 5.110, uses the check-jsonschema CLI tool to ensure that all files are valid JSON and
+  match the KPluginMetaData spec. This only applied if the JSON file has a "KPlugin" object in its root.
+  To ignore invalid files, for example for testing error handling, given files can be exlcuded in the .kde-ci.yml file
+  Define Options.json-validate-ignore with an array of the files you want to ignore
 
 Example usage:
 
 .. code-block:: cmake
 
   include(KDEGitCommitHooks)
-  kde_configure_git_pre_commit_hook(CHECKS CLANG_FORMAT)
+  kde_configure_git_pre_commit_hook(CHECKS JSON_SCHEMA CLANG_FORMAT)
 
 Since 5.79
 #]=======================================================================]
 
 # try to find clang-format in path
 find_program(KDE_CLANG_FORMAT_EXECUTABLE clang-format)
+find_program(KDE_CHECK_JSONSCHEMA_EXECUTABLE check-jsonschema)
 set(PRE_COMMIT_HOOK_UNIX "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/pre-commit.in")
 set(CLANG_FORMAT_UNIX "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/clang-format.sh")
+set(JSON_SCHEMA_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/json-schema.py")
+set(JSON_SCHEMA_IN "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/combined.schema.json.in")
+set(GIT_HOOKS_DIR "${CMAKE_SOURCE_DIR}/.git/hooks")
+set(JSON_SCHEMA_OUT "${GIT_HOOKS_DIR}/scripts/combined.schema.json")
 
 function(KDE_CONFIGURE_GIT_PRE_COMMIT_HOOK)
     set(_oneValueArgs "")
     set(_multiValueArgs CHECKS CUSTOM_SCRIPTS)
-    cmake_parse_arguments(ARG "" "${_oneValueArgs}" "${_multiValueArgs}" ${ARGN} )
+    cmake_parse_arguments(ARG "" "${_oneValueArgs}" "${_multiValueArgs}" ${ARGN})
 
     if(NOT CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
         message(STATUS "Project is not top level project - pre-commit hook not installed")
@@ -63,13 +72,12 @@ function(KDE_CONFIGURE_GIT_PRE_COMMIT_HOOK)
         message(FATAL_ERROR "No checks were specified")
     endif()
 
-    set(GIT_HOOKS_DIR "${CMAKE_SOURCE_DIR}/.git/hooks")
     find_package(Git QUIET)
 
     if (NOT IS_DIRECTORY ${GIT_HOOKS_DIR} # In case of tarballs there is no .git directory
         OR NOT (UNIX OR WIN32)
         OR NOT GIT_FOUND
-    )
+        )
         return()
     endif()
 
@@ -80,23 +88,20 @@ function(KDE_CONFIGURE_GIT_PRE_COMMIT_HOOK)
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    if(_gitresult EQUAL 0
-        AND NOT ${GIT_HOOKS_DIR} EQUAL "${_gitoutput}"
-    )
+    if(_gitresult EQUAL 0 AND NOT ${GIT_HOOKS_DIR} EQUAL "${_gitoutput}")
         message(WARNING "Git is configured to use '${_gitoutput}' for hooks. The generated commit hooks will likely not be executed.")
     endif()
 
-    if (COMMAND KDE_CLANG_FORMAT)
-        set(HAS_CLANG_FORMAT_COMMAND_INCLUDED TRUE)
-    else()
-        set(HAS_CLANG_FORMAT_COMMAND_INCLUDED FALSE)
-    endif()
-
     string(REPLACE ";" "\n" PRE_COMMIT_SCRIPTS "${ARG_CUSTOM_SCRIPTS}")
-
     set(_write_hook FALSE)
     if(KDE_CLANG_FORMAT_EXECUTABLE)
         list(FIND ARG_CHECKS "CLANG_FORMAT" _index)
+        # Used to configure clang-format.sh script
+        if (COMMAND KDE_CLANG_FORMAT)
+            set(HAS_CLANG_FORMAT_COMMAND_INCLUDED TRUE)
+        else()
+            set(HAS_CLANG_FORMAT_COMMAND_INCLUDED FALSE)
+        endif()
         if (${_index} GREATER -1)
             set(CLANG_FORMAT_SCRIPT "\"$(git rev-parse --git-common-dir)\"/hooks/scripts/clang-format.sh")
             configure_file(${CLANG_FORMAT_UNIX} "${GIT_HOOKS_DIR}/scripts/clang-format.sh" @ONLY)
@@ -105,39 +110,59 @@ function(KDE_CONFIGURE_GIT_PRE_COMMIT_HOOK)
         endif()
     endif()
 
+    list(FIND ARG_CHECKS "JSON_SCHEMA" _index)
+    if (${_index} GREATER -1)
+      foreach(path ${CMAKE_PREFIX_PATH};${CMAKE_INCLUDE_PATH})
+          file(GLOB files "${path}/${KDE_INSTALL_DATADIR}/kf6/jsonschema/*.json") 
+          set(SCHEMA_FILES ${SCHEMA_FILES};${files})
+      endforeach()
+        foreach (SCHEMA ${SCHEMA_FILES})
+            if (SCHEMA_INCLUDES)
+                set(SCHEMA_INCLUDES "${SCHEMA_INCLUDES},")
+            endif()
+            set(SCHEMA_INCLUDES "${SCHEMA_INCLUDES}{\"$ref\": \"${SCHEMA}\"}")
+        endforeach()
+        configure_file(${JSON_SCHEMA_IN} ${JSON_SCHEMA_OUT} @ONLY)
+        if (KDE_CHECK_JSONSCHEMA_EXECUTABLE)
+            set(PRE_COMMIT_SCRIPTS "set -e\npython3 ${JSON_SCHEMA_SCRIPT} \"${JSON_SCHEMA_OUT}\"\n${PRE_COMMIT_SCRIPTS}")
+        else()
+            message(WARNING "check-jsonschema executable not found. Please install it using pip or using your package manager")
+        endif()
+    endif()
+
 
     set(_hook_file "${GIT_HOOKS_DIR}/pre-commit")
     # Appending to the file is quite ugly and causes edge cases. With the CUSTOM_SCRIPTS arg, this should not be needed though
     if (ECM_GLOBAL_FIND_VERSION VERSION_GREATER_EQUAL 5.109.0)
-      set(PRE_COMMIT_SCRIPTS "# This file is autogenerated by kde_configure_pre_commit_hook, any changes will be lost! \n${PRE_COMMIT_SCRIPTS}")
-      configure_file(${PRE_COMMIT_HOOK_UNIX} "${_hook_file}")
+        set(PRE_COMMIT_SCRIPTS "# This file is autogenerated by kde_configure_pre_commit_hook, any changes will be lost! \n${PRE_COMMIT_SCRIPTS}")
+        configure_file(${PRE_COMMIT_HOOK_UNIX} "${_hook_file}")
     else()
-      if(NOT _write_hook)
-          return()
-          message(WARNING "No clang-format executable was found, skipping the formatting pre-commit hook")
-      endif()
-      # Doesn't exist? write away
-      if(NOT EXISTS ${_hook_file})
-          configure_file(${PRE_COMMIT_HOOK_UNIX} "${_hook_file}")
-          return()
-      endif()
+        if(NOT _write_hook)
+            return()
+            message(WARNING "No clang-format executable was found, skipping the formatting pre-commit hook")
+        endif()
+        # Doesn't exist? write away
+        if(NOT EXISTS ${_hook_file})
+            configure_file(${PRE_COMMIT_HOOK_UNIX} "${_hook_file}")
+            return()
+        endif()
 
-      file(READ ${_hook_file} _contents)
+        file(READ ${_hook_file} _contents)
 
-      # For when CLANG_FORMAT_SCRIPT didn't have the 'git rev-parse --git-common-dir' part
-      set(_old_cmd "./.git/hooks/scripts/clang-format.sh")
-      string(FIND "${_contents}" "${_old_cmd}" _idx)
-      if (${_idx} GREATER -1)
-          string(REPLACE "${_old_cmd}" "${CLANG_FORMAT_SCRIPT}" _contents "${_contents}")
-          file(WRITE ${_hook_file} "${_contents}")
-          return()
-      endif()
+        # For when CLANG_FORMAT_SCRIPT didn't have the 'git rev-parse --git-common-dir' part
+        set(_old_cmd "./.git/hooks/scripts/clang-format.sh")
+        string(FIND "${_contents}" "${_old_cmd}" _idx)
+        if (${_idx} GREATER -1)
+            string(REPLACE "${_old_cmd}" "${CLANG_FORMAT_SCRIPT}" _contents "${_contents}")
+            file(WRITE ${_hook_file} "${_contents}")
+            return()
+        endif()
 
-      string(FIND "${_contents}" "${CLANG_FORMAT_SCRIPT}" _idx)
-      # File exists and doesn't have the clang-format.sh line, append it
-      # so as to not overwrite users' customisations
-      if (_idx EQUAL -1)
-          file(APPEND ${_hook_file} "${CLANG_FORMAT_SCRIPT}")
-      endif()
+        string(FIND "${_contents}" "${CLANG_FORMAT_SCRIPT}" _idx)
+        # File exists and doesn't have the clang-format.sh line, append it
+        # so as to not overwrite users' customisations
+        if (_idx EQUAL -1)
+            file(APPEND ${_hook_file} "${CLANG_FORMAT_SCRIPT}")
+        endif()
     endif()
 endfunction()
