1829894a1ea4a30ac4d1a9ead55ccf3059486474
Ingo Klöcker <dev@ingo-kloecker.de>
2023-09-27 09:41:29 +0200

    Run clang-format on the stage instead of the working dir
    This has two effects:
    a) It's way faster because only the staged files are checked.
    b) More importantly, it prevents you from committing wrongly formatted
    code that you have clang-formatted in the working directory after the
    commit hook complained but that you forgot to stage after fixing the
    formatting.

--- kde-modules/kde-git-commit-hooks/clang-format.sh
+++ kde-modules/kde-git-commit-hooks/clang-format.sh
@@ -2,7 +2,7 @@
 
 # Based on okular/hooks/pre-commit, credits go to Albert Astals Cid
 
-readonly output=$(git clang-format --extensions 'cpp,h,hpp,c' -v --diff)
+readonly output=$(git clang-format --staged --extensions 'cpp,h,hpp,c' -v --diff)
 
 if [[ ! -f .clang-format ]]; then
     if [[ @HAS_CLANG_FORMAT_COMMAND_INCLUDED@ = TRUE ]]; then
@@ -19,6 +19,6 @@ if [[ "$output" == *"no modified files to format"* ]]; then exit 0; fi
 if [[ "$output" == *"clang-format did not modify any files"* ]]; then exit 0; fi
 
 echo "ERROR: You have unformatted changes, please format your files. You can do this using the following commands:"
-echo "       git clang-format --extensions 'cpp,h,hpp,c' --force # format the changed parts"
-echo "       git clang-format --extensions 'cpp,h,hpp,c' --diff # preview the changes done by the formatter"
+echo "       git clang-format --staged --extensions 'cpp,h,hpp,c' # format the changed parts"
+echo "       git clang-format --staged --extensions 'cpp,h,hpp,c' --diff # preview the changes done by the formatter"
 exit 1
