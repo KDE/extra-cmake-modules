ad895935515dc45121b86bb54eff4269e0cdce21
Thomas Weißschuh <thomas@t-8ch.de>
2024-03-08 07:45:07 +0100

    Avoid spurious message about appstreamtest
    When the function appstreamtest() is called multiple times a warning
    message about the missing appstreamcli is printed although the test was
    configured correctly.

--- kde-modules/KDECMakeSettings.cmake
+++ kde-modules/KDECMakeSettings.cmake
@@ -162,7 +162,9 @@ find_program(APPSTREAMCLI appstreamcli)
 function(appstreamtest)
     cmake_policy(PUSH)
     cmake_policy(SET CMP0064 NEW) # enable TEST operator
-    if(APPSTREAMCLI AND NOT TEST appstreamtest)
+    if(TEST appstreamtest)
+        # already configured
+    elseif(APPSTREAMCLI)
         add_test(NAME appstreamtest COMMAND ${CMAKE_COMMAND} -DAPPSTREAMCLI=${APPSTREAMCLI} -DINSTALL_FILES=${CMAKE_BINARY_DIR}/install_manifest.txt -P ${CMAKE_CURRENT_LIST_DIR}/appstreamtest.cmake)
     else()
         message(STATUS "Could not set up the appstream test. appstreamcli is missing.")
