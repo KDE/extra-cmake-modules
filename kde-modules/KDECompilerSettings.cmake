9dac4f87cb611825c133f4b73d1260cce677eaf7
Nicolas Fella <nicolas.fella@gmx.de>
2024-11-25 22:28:29 +0100

    KDECompilerSettings: Build with -fhardened if available
    This enables a bunch of (security) hardening features in GCC,
    some of which are already applied in downstream builds.
    
    Add it here so that developer builds also benefit from it

--- kde-modules/KDECompilerSettings.cmake
+++ kde-modules/KDECompilerSettings.cmake
@@ -609,6 +609,12 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
    endif()
 endif()
 
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+   if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 14.0.0)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fhardened" )
+   endif()
+endif()
+
 # -w1 turns on warnings and errors
 # FIXME: someone needs to have a closer look at the Intel compiler options
 if (CMAKE_C_COMPILER_ID STREQUAL "Intel" AND NOT WIN32)
