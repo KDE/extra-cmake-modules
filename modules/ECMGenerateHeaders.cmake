4dfd6834b0613012fa24a3295971011442ac4bce
Friedrich W. H. Kossebau <kossebau@kde.org>
2025-08-13 15:54:11 +0200

    ecm_generate_headers: add SHARED_PREFIX argument
    A variant of PREFIX, intended to serve semantically the use-case for
    namespaced forwarding headers whiere the original headers are deployed
    in the very same installation subdirectory, not a separate one.
    It should replace the approach by consumers to achieve the purpose by
    not setting PREFIX, but passing a custom OUTPUT_DIR with the very prefix
    name. Which lacks the info to the macro of the intension and the very
    prefix name, as well as the support for pre-installation internal builds
    using the forwarding headers.

--- modules/ECMGenerateHeaders.cmake
+++ modules/ECMGenerateHeaders.cmake
@@ -18,6 +18,7 @@ Generate C/C++ CamelCase forwarding headers.
       [HEADER_EXTENSION <header_extension>]
       [OUTPUT_DIR <output_dir>]
       [PREFIX <prefix>]
+      [SHARED_PREFIX <prefix>] # since 6.19
       [REQUIRED_HEADERS <variable>]
       [COMMON_HEADER <HeaderName>]
       [RELATIVE <relative_path>])
@@ -47,6 +48,14 @@ forwarding headers based on the original names (e.g. ``kparts/part.h``).  This
 allows includes like ``"#include <kparts/part.h>"`` to be used before
 installation, as long as the include_directories are set appropriately.
 
+``SHARED_PREFIX`` works similar to ``PREFIX``. It though assumes the original
+headers will be installed in the same subdirectory as the forwarding headers.
+So the generated files will include the original ones locally without any prefix.
+And the above mentioned pre-installation convenience forwarding headers based on
+the original names will be placed in the same subdirectory
+(e.g. ``KParts/part.h``), to allow includes like ``"#include <KParts/Part>"`` to
+be used before installation and working properly. Since 6.19.0.
+
 ``OUTPUT_DIR`` specifies where the files will be generated; this should be within
 the build directory. By default, ``${CMAKE_CURRENT_BINARY_DIR}`` will be used.
 This option can be used to avoid file conflicts.
@@ -65,7 +74,7 @@ The ``RELATIVE`` argument indicates where the original headers can be found
 relative to ``CMAKE_CURRENT_SOURCE_DIR``.  It does not affect the generated
 CamelCase forwarding files, but ``ecm_generate_headers()`` uses it when checking
 that the original header exists, and to generate originally named forwarding
-headers when ``PREFIX`` is set.
+headers when ``PREFIX`` or ``SHARED_PREFIX`` is set.
 
 To allow other parts of the source distribution (eg: tests) to use the
 generated headers before installation, it may be desirable to add to the
@@ -79,7 +88,7 @@ Otherwise you could do (adapt if ``OUTPUT_DIR`` is something else)
 
   target_include_directories(MyLib PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>")
 
-Example usage (without ``PREFIX``):
+Example usage (without ``PREFIX`` pr ``SHARED_PREFIX``):
 
 .. code-block:: cmake
 
@@ -118,17 +127,40 @@ Example usage (with ``PREFIX``):
           DESTINATION ${CMAKE_INSTALL_PREFIX}/include/mylib
           COMPONENT Devel)
 
+Example usage (with ``SHARED_PREFIX``):
+
+.. code-block:: cmake
+
+  ecm_generate_headers(
+      MyLib_FORWARDING_HEADERS
+      HEADERS
+          Foo
+          # several classes are contained in bar.h, so generate
+          # additional files
+          Bar,BarList
+          # etc
+      SHARED_PREFIX MyLib
+      REQUIRED_HEADERS MyLib_HEADERS
+  )
+  install(FILES ${MyLib_FORWARDING_HEADERS} ${MyLib_HEADERS}
+          DESTINATION ${CMAKE_INSTALL_PREFIX}/include/MyLib
+          COMPONENT Devel)
+
 Since pre-1.0.0.
 #]=======================================================================]
 
 function(ECM_GENERATE_HEADERS camelcase_forwarding_headers_var)
     set(options)
-    set(oneValueArgs ORIGINAL HEADER_EXTENSION OUTPUT_DIR PREFIX REQUIRED_HEADERS COMMON_HEADER RELATIVE)
+    set(oneValueArgs ORIGINAL HEADER_EXTENSION OUTPUT_DIR PREFIX SHARED_PREFIX REQUIRED_HEADERS COMMON_HEADER RELATIVE)
     set(multiValueArgs HEADER_NAMES)
     cmake_parse_arguments(EGH "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if (EGH_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "Unexpected arguments to ECM_GENERATE_HEADERS: ${EGH_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(EGH_PREFIX AND EGH_SHARED_PREFIX)
+       message(FATAL_ERROR "Cannot pass both PREFIX and SHARED_PREFIX arguments to ECM_GENERATE_HEADERS")
     endif()
 
     if(NOT EGH_HEADER_NAMES)
@@ -156,16 +188,27 @@ function(ECM_GENERATE_HEADERS camelcase_forwarding_headers_var)
         set(EGH_RELATIVE "${EGH_RELATIVE}/")
     endif()
 
-    set(originalprefix)
-    if (EGH_PREFIX)
-        if (NOT "${EGH_PREFIX}" MATCHES "^.*/$")
-            set(EGH_PREFIX "${EGH_PREFIX}/")
-        endif()
+    set(prefix)
+    if (EGH_SHARED_PREFIX)
+        set(prefix "${EGH_SHARED_PREFIX}")
+    elseif (EGH_PREFIX)
+        set(prefix "${EGH_PREFIX}")
+    endif()
+    if (prefix AND NOT "${prefix}" MATCHES "^.*/$")
+        string(APPEND prefix "/")
+    endif()
+
+    set(originalprefix) # prefix used in the include by the forwarding header
+    set(buildoriginalprefix) # prefix used in the build to place the convenience forwarding headers
+    if (EGH_SHARED_PREFIX)
+        set(buildoriginalprefix "${prefix}")
+    elseif (EGH_PREFIX)
         if (EGH_ORIGINAL STREQUAL "CAMELCASE")
-            set(originalprefix "${EGH_PREFIX}")
+            set(originalprefix "${prefix}")
         else()
-            string(TOLOWER "${EGH_PREFIX}" originalprefix)
+            string(TOLOWER "${prefix}" originalprefix)
         endif()
+        set(buildoriginalprefix "${originalprefix}")
     endif()
 
     foreach(_classnameentry ${EGH_HEADER_NAMES})
@@ -185,21 +228,23 @@ function(ECM_GENERATE_HEADERS camelcase_forwarding_headers_var)
         endif()
 
         foreach(_CLASSNAME ${_classnames})
-            set(FANCY_HEADER_FILE "${EGH_OUTPUT_DIR}/${EGH_PREFIX}${_CLASSNAME}")
+            set(FANCY_HEADER_FILE "${EGH_OUTPUT_DIR}/${prefix}${_CLASSNAME}")
             if (NOT EXISTS ${FANCY_HEADER_FILE})
                 file(WRITE ${FANCY_HEADER_FILE} "#include \"${originalprefix}${originalbasename}.${EGH_HEADER_EXTENSION}\" // IWYU pragma: export\n")
             endif()
             list(APPEND ${camelcase_forwarding_headers_var} "${FANCY_HEADER_FILE}")
-            if (EGH_PREFIX)
-                # Local forwarding header, for namespaced headers, e.g. kparts/part.h
+            if (prefix)
+                # Build-local convenience forwarding header, for namespaced headers, e.g. kparts/part.h
+                # Ensures either the prefixed forwarding include works ("kparts/part.h"), or the unnamespaced
+                # local relative ("part.h") will pick up the correct header, not some consumer one.
                 if(EGH_ORIGINAL STREQUAL "CAMELCASE")
                     set(originalclassname "${_CLASSNAME}")
                 else()
                     string(TOLOWER "${_CLASSNAME}" originalclassname)
                 endif()
-                set(REGULAR_HEADER_NAME ${EGH_OUTPUT_DIR}/${originalprefix}${originalclassname}.${EGH_HEADER_EXTENSION})
+                set(REGULAR_HEADER_NAME ${EGH_OUTPUT_DIR}/${buildoriginalprefix}${originalclassname}.${EGH_HEADER_EXTENSION})
                 if (NOT EXISTS ${REGULAR_HEADER_NAME})
-                    file(RELATIVE_PATH _actualheader_relative "${EGH_OUTPUT_DIR}/${originalprefix}" "${_actualheader}")
+                    file(RELATIVE_PATH _actualheader_relative "${EGH_OUTPUT_DIR}/${buildoriginalprefix}" "${_actualheader}")
                     file(WRITE ${REGULAR_HEADER_NAME} "#include \"${_actualheader_relative}\" // IWYU pragma: export\n")
                 endif()
             endif()
@@ -210,7 +255,7 @@ function(ECM_GENERATE_HEADERS camelcase_forwarding_headers_var)
 
     if(EGH_COMMON_HEADER)
         #combine required headers into 1 big convenience header
-        set(COMMON_HEADER ${EGH_OUTPUT_DIR}/${EGH_PREFIX}${EGH_COMMON_HEADER})
+        set(COMMON_HEADER ${EGH_OUTPUT_DIR}/${prefix}${EGH_COMMON_HEADER})
         file(WRITE ${COMMON_HEADER} "// convenience header\n")
         foreach(_header ${_REQUIRED_HEADERS})
             get_filename_component(_base ${_header} NAME)
