3f76d71045e935ced4723c12345162f16cc8cb21
Max Brazhnikov <makc@FreeBSD.org>
2025-12-15 17:08:25 +0300

    FindInotify: fix comparison

--- find-modules/FindInotify.cmake
+++ find-modules/FindInotify.cmake
@@ -56,7 +56,7 @@ if(Inotify_INCLUDE_DIRS)
         cmake_path(GET Inotify_INCLUDE_DIRS PARENT_PATH includes_prefix)
         cmake_path(GET Inotify_LIBRARIES PARENT_PATH lib_prefix)
         cmake_path(GET lib_prefix PARENT_PATH lib_prefix)
-        cmake_path(COMPARE includes_prefix EQUAL lib_prefix prefixes_match)
+        cmake_path(COMPARE ${includes_prefix} EQUAL ${lib_prefix} prefixes_match)
 
         if(NOT prefixes_match)
             set(Inotify_FOUND TRUE)
