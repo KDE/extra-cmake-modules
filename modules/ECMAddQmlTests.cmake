c863f4516a072ef41ff81ac6e63def4897a7156f
Thiago Sueto <herzenschein@gmail.com>
2026-06-02 23:15:25 -0300

    Draft ECMAddQmlTests

--- modules/ECMAddQmlTests.cmake
+++ modules/ECMAddQmlTests.cmake
@@ -1,12 +1,8 @@
 function(ecm_add_qml_tests)
-    find_package(Qt6QuickTest REQUIRED)
-    file(REMOVE "${PROJECT_BINARY_DIR}/_qmltest.cpp")
-    file(WRITE "${PROJECT_BINARY_DIR}/_qmltest.cpp" "#include <quicktest.h>\nQUICK_TEST_MAIN(_qmltest)")
-    add_executable(_qmltest "${PROJECT_BINARY_DIR}/_qmltest.cpp")
-    target_link_libraries(_qmltest PRIVATE Qt6::QuickTest)
+    find_package(Qt6 REQUIRED COMPONENTS QuickTest QmlTools CONFIG QUIET)
     foreach(_test ${ARGV})
         add_test(NAME ${_test}
-            COMMAND ${PROJECT_BINARY_DIR}/bin/_qmltest
+            COMMAND Qt6::qmltestrunner
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     endforeach()
